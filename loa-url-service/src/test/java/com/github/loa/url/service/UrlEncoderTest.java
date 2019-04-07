6a77109b11379cfcb631ac80aa354d244a59008b
212675392 <gyula.lakatos1@ge.com>
2019-04-07 16:46:03 +0200

    #7 - Added basic unit tests for URL encoder.

---
+++ loa-url-service/src/test/java/com/github/loa/url/service/UrlEncoderTest.java
@@ -0,0 +1,33 @@
+package com.github.loa.url.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UrlEncoderTest {
+
+    private UrlEncoder urlEncoder;
+
+    @BeforeEach
+    private void setup() {
+        urlEncoder = new UrlEncoder();
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+            value = {
+                    "http://www.example.com/?test=Hello world,http://www.example.com/?test=Hello%20world",
+                    "http://www.example.com/?test=ŐÚőúŰÜűü,http://www.example.com/?test=%C5%90%C3%9A%C5%91%C3%BA%C5%B0%C3%9C%C5%B1%C3%BC"
+            }
+    )
+    void encode(final String urlToEncode, final String expected) throws MalformedURLException {
+        final URL encoded = urlEncoder.encode(new URL(urlToEncode));
+
+        assertEquals(expected, encoded.toString());
+    }
+}
