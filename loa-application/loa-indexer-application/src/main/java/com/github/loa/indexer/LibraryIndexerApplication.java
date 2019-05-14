66869bd996e019a1010be795878b30f03f31ae8c
212675392 <gyula.lakatos1@ge.com>
2019-05-14 16:40:31 +0200

    #36 - Fixed that MongoDB was always connected to localhost.

--- loa-application/loa-indexer-application/src/main/java/com/github/loa/indexer/LibraryIndexerApplication.java
+++ loa-application/loa-indexer-application/src/main/java/com/github/loa/indexer/LibraryIndexerApplication.java
@@ -1,13 +1,13 @@
 package com.github.loa.indexer;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.mongo.MongoRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@Slf4j
-@RequiredArgsConstructor
-@SpringBootApplication(scanBasePackages = "com.github.loa")
+@SpringBootApplication(scanBasePackages = "com.github.loa", exclude = {MongoAutoConfiguration.class,
+        MongoRepositoriesAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class LibraryIndexerApplication {
 
     public static void main(final String[] args) {
