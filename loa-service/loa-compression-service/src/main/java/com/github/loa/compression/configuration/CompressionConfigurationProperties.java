8875b0cdb6409869dfad14677e2850a46c1309f6
212675392 <gyula.lakatos1@ge.com>
2019-05-19 16:35:47 +0200

    #42 - Added javadocs to CompressionConfigurationProperties.

--- loa-service/loa-compression-service/src/main/java/com/github/loa/compression/configuration/CompressionConfigurationProperties.java
+++ loa-service/loa-compression-service/src/main/java/com/github/loa/compression/configuration/CompressionConfigurationProperties.java
@@ -5,6 +5,9 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+/**
+ * Contains the configuration properties for the compression.
+ */
 @Data
 @Component
 @ConfigurationProperties("loa.compression")
