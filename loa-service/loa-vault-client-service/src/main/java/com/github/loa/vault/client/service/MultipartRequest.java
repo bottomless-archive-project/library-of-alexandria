0a49c8fc6cfd4fb883c45624473adcaff0efa5a4
212675392 <gyula.lakatos1@ge.com>
2019-05-24 17:23:08 +0200

    #49 - Increased the timeout for saving items to the vault.

--- loa-service/loa-vault-client-service/src/main/java/com/github/loa/vault/client/service/MultipartRequest.java
+++ loa-service/loa-vault-client-service/src/main/java/com/github/loa/vault/client/service/MultipartRequest.java
@@ -4,14 +4,17 @@ import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.Charset;
+import java.util.concurrent.TimeUnit;
 
 public class MultipartRequest {
 
-    private final String boundary;
+    private static final int UPLOAD_TIMEOUT = (int) TimeUnit.MINUTES.toMillis(30);
     private static final String LINE_FEED = "\r\n";
-    private HttpURLConnection httpURLConnection;
-    private OutputStream outputStream;
-    private PrintWriter writer;
+
+    private final String boundary;
+    private final HttpURLConnection httpURLConnection;
+    private final OutputStream outputStream;
+    private final PrintWriter writer;
 
     /**
      * This constructor initializes a new HTTP POST request with content type
@@ -25,11 +28,11 @@ public class MultipartRequest {
         httpURLConnection.setUseCaches(false);
         httpURLConnection.setDoOutput(true);
         httpURLConnection.setDoInput(true);
+        httpURLConnection.setConnectTimeout(UPLOAD_TIMEOUT); // It takes time to save lots of data to disk for the vault
         httpURLConnection.setRequestProperty("Content-Type",
                 "multipart/form-data; boundary=" + boundary);
         outputStream = httpURLConnection.getOutputStream();
-        writer = new PrintWriter(new OutputStreamWriter(outputStream, Charset.forName("UTF-8")),
-                true);
+        writer = new PrintWriter(new OutputStreamWriter(outputStream, Charset.forName("UTF-8")), true);
     }
 
     /**
@@ -39,9 +42,9 @@ public class MultipartRequest {
      * @param uploadFile a File to be uploaded
      */
     public void addFilePart(String fieldName, InputStream uploadFile) throws IOException {
-        writer.append("--" + boundary).append(LINE_FEED);
-        writer.append("Content-Disposition: form-data; name=\"" + fieldName + "\"; filename=\"" + fieldName + "\"")
-                .append(LINE_FEED);
+        writer.append("--").append(boundary).append(LINE_FEED);
+        writer.append("Content-Disposition: form-data; name=\"").append(fieldName).append("\"; filename=\"")
+                .append(fieldName).append("\"").append(LINE_FEED);
         writer.append("Content-Type: application/pdf").append(LINE_FEED);
         writer.append("Content-Transfer-Encoding: binary").append(LINE_FEED);
         writer.append(LINE_FEED);
@@ -63,7 +66,7 @@ public class MultipartRequest {
      */
     public int execute() throws IOException {
         writer.append(LINE_FEED).flush();
-        writer.append("--" + boundary + "--").append(LINE_FEED);
+        writer.append("--").append(boundary).append("--").append(LINE_FEED);
         writer.close();
 
         return httpURLConnection.getResponseCode();
--- loa-service/loa-vault-client-service/src/main/java/com/github/loa/vault/client/service/VaultClientService.java
+++ loa-service/loa-vault-client-service/src/main/java/com/github/loa/vault/client/service/VaultClientService.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Service;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
