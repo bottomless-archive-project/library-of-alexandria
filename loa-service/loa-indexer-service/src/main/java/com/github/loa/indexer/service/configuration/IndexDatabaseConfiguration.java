c8a61f773ddc2b97345b37135c83aa90762d1dff
Laxika <laxika91@gmail.com>
2019-05-27 10:32:31 +0200

    #49 - Increased indexer request timeouts.

--- loa-service/loa-indexer-service/src/main/java/com/github/loa/indexer/service/configuration/IndexDatabaseConfiguration.java
+++ loa-service/loa-indexer-service/src/main/java/com/github/loa/indexer/service/configuration/IndexDatabaseConfiguration.java
@@ -2,11 +2,15 @@ package com.github.loa.indexer.service.configuration;
 
 import lombok.RequiredArgsConstructor;
 import org.apache.http.HttpHost;
+import org.apache.http.client.config.RequestConfig;
 import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 @RequiredArgsConstructor
@@ -22,6 +26,13 @@ public class IndexDatabaseConfiguration {
 
     @Bean
     public RestHighLevelClient restHighLevelClient(final HttpHost httpHost) {
-        return new RestHighLevelClient(RestClient.builder(httpHost));
+        return new RestHighLevelClient(
+                RestClient.builder(httpHost)
+                        .setRequestConfigCallback(
+                                requestConfigBuilder -> requestConfigBuilder
+                                        .setConnectTimeout((int) TimeUnit.MINUTES.toMillis(1))
+                                        .setSocketTimeout((int) TimeUnit.MINUTES.toMillis(30))
+                        )
+        );
     }
 }
