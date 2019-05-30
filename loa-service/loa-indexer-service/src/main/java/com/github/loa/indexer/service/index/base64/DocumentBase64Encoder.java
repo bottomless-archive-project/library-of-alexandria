35924a6a5895e6e96e3299491b8d368667e2cb43
Laxika <laxika91@gmail.com>
2019-05-30 22:46:14 +0200

    #54 - Fixed a compile error.

--- loa-service/loa-indexer-service/src/main/java/com/github/loa/indexer/service/index/base64/DocumentBase64Encoder.java
+++ loa-service/loa-indexer-service/src/main/java/com/github/loa/indexer/service/index/base64/DocumentBase64Encoder.java
@@ -5,7 +5,6 @@ import com.github.loa.vault.client.service.VaultClientService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
 import java.io.InputStream;
 
 /**
@@ -27,10 +26,6 @@ public class DocumentBase64Encoder {
     public String encodeDocument(final DocumentEntity documentEntity) {
         final InputStream inputStream = vaultClientService.queryDocument(documentEntity);
 
-        try {
-            return base64Encoder.encode(inputStream);
-        } catch (IOException e) {
-            throw new RuntimeException("Unable to base64 encode document " + documentEntity.getId() + "!", e);
-        }
+        return base64Encoder.encode(inputStream);
     }
 }
