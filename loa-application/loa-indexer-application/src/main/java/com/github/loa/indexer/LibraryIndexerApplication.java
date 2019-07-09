d3cc46d504ffd0d9ad71e1d4402799b8b50da885
Laxika <laxika91@gmail.com>
2019-07-09 21:04:46 +0200

    #60 - Updated the indexer application.

--- loa-application/loa-indexer-application/src/main/java/com/github/loa/indexer/LibraryIndexerApplication.java
+++ loa-application/loa-indexer-application/src/main/java/com/github/loa/indexer/LibraryIndexerApplication.java
@@ -1,12 +1,10 @@
 package com.github.loa.indexer;
 
-import com.morethanheroic.taskforce.executor.JobExecutor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.mongo.MongoRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(scanBasePackages = "com.github.loa", exclude = {MongoAutoConfiguration.class,
         MongoRepositoriesAutoConfiguration.class, MongoDataAutoConfiguration.class})
@@ -14,10 +12,5 @@ public class LibraryIndexerApplication {
 
     public static void main(final String[] args) {
         SpringApplication.run(LibraryIndexerApplication.class, args);
-    }
-
-    @Bean
-    public JobExecutor jobExecutor() {
-        return new JobExecutor();
     }
 }
--- loa-application/loa-indexer-application/src/main/java/com/github/loa/indexer/command/IndexerCommand.java
+++ loa-application/loa-indexer-application/src/main/java/com/github/loa/indexer/command/IndexerCommand.java
@@ -1,11 +1,7 @@
 package com.github.loa.indexer.command;
 
-import com.github.loa.indexer.batch.generator.DownloadedDocumentEntityGenerator;
-import com.github.loa.indexer.batch.task.IndexerTask;
 import com.morethanheroic.taskforce.executor.JobExecutor;
 import com.morethanheroic.taskforce.job.Job;
-import com.morethanheroic.taskforce.job.builder.JobBuilder;
-import com.morethanheroic.taskforce.sink.DiscardingSink;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
@@ -17,51 +13,14 @@ import org.springframework.stereotype.Component;
 public class IndexerCommand implements CommandLineRunner {
 
     private final JobExecutor jobExecutor;
-    private final DownloadedDocumentEntityGenerator downloadedDocumentEntityGenerator;
-    private final IndexerTask indexerTask;
+    private final Job indexerJob;
 
     @Override
     public void run(final String... args) {
         log.info("Initializing document indexing.");
 
-        final Job job = JobBuilder.newBuilder()
-                .generator(downloadedDocumentEntityGenerator)
-                .task(indexerTask)
-                .sink(DiscardingSink.of())
-                .withThreadCount(1)
-                .build();
-
-        jobExecutor.execute(job);
+        jobExecutor.execute(indexerJob);
 
         log.info("Finished document indexing.");
-
-        /*
-        try {
-            while (true) {
-                final List<DocumentEntity> documentEntities = documentEntityFactory
-                        .getDocumentEntity(DocumentStatus.DOWNLOADED);
-
-                if (documentEntities.isEmpty()) {
-                    log.info("Waiting for a while because no documents are available for indexing.");
-
-                    Thread.sleep(indexerConfigurationProperties.getSleepTime());
-                } else {
-                    for (DocumentEntity documentEntity : documentEntities) {
-                        if (documentEntity.getFileSize() < indexerConfigurationProperties.getMaximumFileSize()) {
-                            indexerService.indexDocuments(documentEntity);
-                        } else {
-                            log.info("Skipping document " + documentEntity.getId() + " because it's size is too high.");
-
-                            documentManipulator.markIndexFailure(documentEntity.getId());
-                        }
-                    }
-
-                    log.info("Indexed " + documentEntities.size() + " documents!");
-                }
-            }
-        } catch (InterruptedException e) {
-            log.error("Failed to index documents!", e);
-        }
-        */
     }
 }
