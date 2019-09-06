5bc5abd0731aa83ac08d9dc32ef87dbfab061f6e
212675392 <gyula.lakatos1@ge.com>
2019-09-06 14:38:12 +0200

    #50 - Updated Elasticsearch version to 7.3.1.

--- loa-application/loa-downloader-application/src/main/java/com/github/loa/downloader/LibraryDownloaderApplication.java
+++ loa-application/loa-downloader-application/src/main/java/com/github/loa/downloader/LibraryDownloaderApplication.java
@@ -5,7 +5,6 @@ import io.micrometer.core.instrument.logging.LoggingMeterRegistry;
 import io.micrometer.core.instrument.logging.LoggingRegistryConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.mongo.MongoRepositoriesAutoConfiguration;
