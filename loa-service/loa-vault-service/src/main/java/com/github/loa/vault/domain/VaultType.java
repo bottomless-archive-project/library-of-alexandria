19686367cfe2b23ed35fc422fca3fd7a9122f25b
212675392 <gyula.lakatos1@ge.com>
2019-09-14 22:32:06 +0200

    #77 - Removed the SMB support from the vault application.

--- loa-service/loa-vault-service/src/main/java/com/github/loa/vault/domain/VaultType.java
+++ loa-service/loa-vault-service/src/main/java/com/github/loa/vault/domain/VaultType.java
@@ -5,9 +5,5 @@ public enum VaultType {
     /**
      * The vault location is directly on the disks.
      */
-    FILE,
-    /**
-     * The vault location is somewhere on the network communicating over SMB (Server Message Block).
-     */
-    SMB
+    FILE
 }
