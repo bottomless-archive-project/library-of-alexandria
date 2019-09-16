56d17f538224000e67309116364926f9bda7bbc4
212675392 <gyula.lakatos1@ge.com>
2019-09-16 17:02:23 +0200

    #78 - Disable the _class property in mongo.

---
+++ loa-service/loa-document-repository/src/main/java/com/github/loa/document/repository/configuration/MongoConfiguration.java
@@ -0,0 +1,24 @@
+package com.github.loa.document.repository.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
+
+@Configuration
+public class MongoConfiguration {
+
+    @Bean
+    public MongoTemplate mongoTemplsate(final MongoDbFactory mongoDbFactory, final MongoMappingContext context) {
+        final MappingMongoConverter converter = new MappingMongoConverter(
+                new DefaultDbRefResolver(mongoDbFactory), context);
+
+        converter.setTypeMapper(new DefaultMongoTypeMapper(null));
+
+        return new MongoTemplate(mongoDbFactory, converter);
+    }
+}
