d99ae5cdbe8b2036b71c5c3bf68f7b3f9f60a05d
laxika <laxika91@gmail.com>
2019-11-13 16:09:41 +0100

    #97 - Small refactoring on the downloader configuration.

--- loa-application/loa-downloader-application/src/main/java/com/github/loa/downloader/configuration/DownloaderConfiguration.java
+++ loa-application/loa-downloader-application/src/main/java/com/github/loa/downloader/configuration/DownloaderConfiguration.java
@@ -2,6 +2,7 @@ package com.github.loa.downloader.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ClientHttpConnector;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
@@ -10,11 +11,17 @@ import reactor.netty.http.client.HttpClient;
 public class DownloaderConfiguration {
 
     @Bean
-    public WebClient downloaderWebClient() {
+    public WebClient downloaderWebClient(final ClientHttpConnector clientHttpConnector) {
         return WebClient.builder()
-                .clientConnector(new ReactorClientHttpConnector(
-                        HttpClient.create().followRedirect(true)
-                ))
+                .clientConnector(clientHttpConnector)
                 .build();
+    }
+
+    @Bean
+    protected ClientHttpConnector clientHttpConnector() {
+        return new ReactorClientHttpConnector(
+                HttpClient.create()
+                        .followRedirect(true)
+        );
     }
 }
