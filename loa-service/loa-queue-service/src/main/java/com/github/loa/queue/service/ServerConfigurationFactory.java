966b29982ef48455adbf574f6ab3f789a573344b
Laxika <laxika91@gmail.com>
2019-11-27 18:06:17 +0100

    #130 - Fixing that the queue application was only accessible from localhost.

--- loa-service/loa-queue-service/src/main/java/com/github/loa/queue/service/ServerConfigurationFactory.java
+++ loa-service/loa-queue-service/src/main/java/com/github/loa/queue/service/ServerConfigurationFactory.java
@@ -7,9 +7,12 @@ import org.apache.activemq.artemis.core.config.Configuration;
 import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
 import org.apache.activemq.artemis.core.remoting.impl.netty.NettyAcceptorFactory;
 import org.apache.activemq.artemis.core.remoting.impl.netty.NettyConnectorFactory;
+import org.apache.activemq.artemis.core.remoting.impl.netty.TransportConstants;
 import org.apache.activemq.artemis.core.server.embedded.EmbeddedActiveMQ;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
+
+import java.util.Map;
 
 @RequiredArgsConstructor
 @ConditionalOnClass(EmbeddedActiveMQ.class)
@@ -34,7 +37,8 @@ public class ServerConfigurationFactory {
         configuration.setSecurityEnabled(false);
         configuration.addConnectorConfiguration("netty-connector", new TransportConfiguration(
                 NettyConnectorFactory.class.getName()));
-        configuration.addAcceptorConfiguration(new TransportConfiguration(NettyAcceptorFactory.class.getName()));
+        configuration.addAcceptorConfiguration(new TransportConfiguration(NettyAcceptorFactory.class.getName(),
+                Map.of(TransportConstants.HOST_PROP_NAME, "0.0.0.0")));
         configuration.setJournalDirectory(queueConfigurationProperties.getDataDirectory() + "/journal");
         configuration.setBindingsDirectory(queueConfigurationProperties.getDataDirectory() + "/bindings");
         configuration.setLargeMessagesDirectory(queueConfigurationProperties.getDataDirectory() + "/largemessages");
