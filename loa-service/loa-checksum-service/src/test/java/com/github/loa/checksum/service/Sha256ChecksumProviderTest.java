7fa898d1a0152ff323b3665aa13c508b6f59e553
laxika <laxika91@gmail.com>
2019-11-30 13:04:27 +0100

    #132 - Fixed the unit tests.

--- loa-service/loa-checksum-service/src/test/java/com/github/loa/checksum/service/Sha256ChecksumProviderTest.java
+++ loa-service/loa-checksum-service/src/test/java/com/github/loa/checksum/service/Sha256ChecksumProviderTest.java
@@ -5,8 +5,6 @@ import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Path;
 
@@ -24,12 +22,11 @@ class Sha256ChecksumProviderTest {
     }
 
     @Test
-    void testChecksum() throws URISyntaxException, IOException {
+    void testChecksum() throws URISyntaxException {
         final Path checksumTestFile = Path.of(this.getClass().getClassLoader().getResource("checksum_test.txt")
                 .toURI());
 
-        final Mono<String> result = sha256ChecksumProvider.checksum(DOCUMENT_ID, new
-                FileInputStream(checksumTestFile.toFile()).readAllBytes());
+        final Mono<String> result = sha256ChecksumProvider.checksum(DOCUMENT_ID, checksumTestFile);
 
         StepVerifier.create(result)
                 .assertNext(resultValue -> assertEquals("5be0888bbe2087f962fee5748d9cf52e37e4c6a24af79675ff7e1ca0a1b12739", resultValue))
