e95e8b61a6309e11d3a205c4ddd86874fd4f5ab0
laxika <laxika91@gmail.com>
2019-12-04 08:03:07 +0100

    #97 - Removed an unused annotation.

--- loa-application/loa-generator-application/src/main/java/com/github/loa/generator/LibraryGeneratorApplication.java
+++ loa-application/loa-generator-application/src/main/java/com/github/loa/generator/LibraryGeneratorApplication.java
@@ -1,13 +1,11 @@
 package com.github.loa.generator;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * The runner class of the generator application.
  */
-@Slf4j
 @SpringBootApplication(scanBasePackages = "com.github.loa")
 public class LibraryGeneratorApplication {
 
