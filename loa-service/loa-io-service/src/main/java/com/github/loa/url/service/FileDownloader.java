d96dc1ffa51ef6d38cde59486f7d46650c2521f3
laxika <laxika91@gmail.com>
2019-12-30 19:38:03 +0100

    #138 - Added a jitter based retry mechanism when the crawler does too many requests to a host.

--- loa-service/loa-io-service/src/main/java/com/github/loa/url/service/FileDownloader.java
+++ loa-service/loa-io-service/src/main/java/com/github/loa/url/service/FileDownloader.java
@@ -4,13 +4,18 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.core.io.buffer.DataBufferUtils;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.retry.Backoff;
+import reactor.retry.Jitter;
+import reactor.retry.Retry;
 
 import java.net.URL;
 import java.nio.file.Path;
+import java.time.Duration;
 
 /**
  * This service is responsible for downloading files from the internet.
@@ -32,8 +37,21 @@ public class FileDownloader {
         try {
             final Flux<DataBuffer> dataBufferFlux = downloaderWebClient.get()
                     .uri(downloadTarget.toURI())
-                    .retrieve()
-                    .bodyToFlux(DataBuffer.class);
+                    .exchange()
+                    .doOnNext(clientResponse -> {
+                        if (clientResponse.statusCode() == HttpStatus.TOO_MANY_REQUESTS) {
+                            log.info("Too many requests for location: {}. Retrying!", downloadTarget);
+
+                            throw new TooManyRequestException();
+                        }
+                    })
+                    .retryWhen(
+                            Retry.anyOf(TooManyRequestException.class)
+                                    .jitter(Jitter.random())
+                                    .backoff(Backoff.exponential(Duration.ofSeconds(5), Duration.ofMinutes(2), 2, true))
+                                    .retryMax(10)
+                    )
+                    .flatMapMany(clientResponse -> clientResponse.bodyToFlux(DataBuffer.class));
 
             return DataBufferUtils.write(dataBufferFlux, resultLocation)
                     .doOnError(error -> resultLocation.toFile().delete())
@@ -43,5 +61,8 @@ public class FileDownloader {
 
             return Mono.empty();
         }
+    }
+
+    private static class TooManyRequestException extends RuntimeException {
     }
 }
