969dd2fc998915b4d84f51c70f6d0f7281ab4e62
laxika <laxika91@gmail.com>
2020-01-08 19:14:14 +0100

    #97 - Removed two unused exceptions.

--- loa-application/loa-downloader-application/src/main/java/com/github/loa/downloader/service/file/domain/FailedToArchiveException.java
+++
@@ -1,8 +0,0 @@
-package com.github.loa.downloader.service.file.domain;
-
-public class FailedToArchiveException extends RuntimeException {
-
-    public FailedToArchiveException(final String message, final Throwable throwable) {
-        super(message, throwable);
-    }
-}
--- loa-application/loa-downloader-application/src/main/java/com/github/loa/downloader/service/file/domain/FileDownloaderException.java
+++
@@ -1,8 +0,0 @@
-package com.github.loa.downloader.service.file.domain;
-
-public class FileDownloaderException extends Exception {
-
-    public FileDownloaderException(final String message, Throwable throwable) {
-        super(message, throwable);
-    }
-}
