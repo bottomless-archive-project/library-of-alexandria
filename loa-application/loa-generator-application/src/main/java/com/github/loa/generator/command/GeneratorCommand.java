15279ed3c661bc7f8a4ca6aee8363287cb3da59d
laxika <laxika91@gmail.com>
2020-01-10 13:12:53 +0100

    #97 - Fixed an extra unneeded file conversion.

--- loa-application/loa-generator-application/src/main/java/com/github/loa/generator/command/GeneratorCommand.java
+++ loa-application/loa-generator-application/src/main/java/com/github/loa/generator/command/GeneratorCommand.java
@@ -4,8 +4,8 @@ import com.github.loa.location.service.DocumentLocationValidator;
 import com.github.loa.queue.service.QueueManipulator;
 import com.github.loa.queue.service.domain.Queue;
 import com.github.loa.queue.service.domain.message.DocumentLocationMessage;
+import com.github.loa.source.configuration.DocumentSourceConfiguration;
 import com.github.loa.source.service.DocumentLocationFactory;
-import com.github.loa.source.service.DocumentSourceItemFactory;
 import com.github.loa.url.service.UrlEncoder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,26 +19,24 @@ public class GeneratorCommand implements CommandLineRunner {
 
     private final DocumentLocationFactory documentLocationFactory;
     private final DocumentLocationValidator documentLocationValidator;
-    private final DocumentSourceItemFactory documentSourceItemFactory;
+    private final DocumentSourceConfiguration documentSourceConfiguration;
     private final UrlEncoder urlEncoder;
     private final QueueManipulator queueManipulator;
 
     @Override
     public void run(final String... args) {
         log.info("Initializing the document location generating.");
-        log.info("There are {} messages already available in the queue!",
-                queueManipulator.getMessageCount(Queue.DOCUMENT_LOCATION_QUEUE));
 
         queueManipulator.silentlyInitializeQueue(Queue.DOCUMENT_LOCATION_QUEUE);
+        log.info("There are {} messages already available in the queue!",
+                queueManipulator.getMessageCount(Queue.DOCUMENT_LOCATION_QUEUE));
 
         documentLocationFactory.streamLocations()
                 .filter(documentLocationValidator::validDocumentLocation)
                 .flatMap(urlEncoder::encode)
-                //TODO: We don't need to double convert!
-                .map(documentSourceItemFactory::newDocumentSourceItem)
-                .map(documentSourceItem -> DocumentLocationMessage.builder()
-                        .sourceName(documentSourceItem.getSourceName())
-                        .documentLocation(documentSourceItem.getDocumentLocation().toString())
+                .map(documentLocation -> DocumentLocationMessage.builder()
+                        .sourceName(documentSourceConfiguration.getName())
+                        .documentLocation(documentLocation.toString())
                         .build()
                 )
                 .doOnNext(this::sendMessage)
