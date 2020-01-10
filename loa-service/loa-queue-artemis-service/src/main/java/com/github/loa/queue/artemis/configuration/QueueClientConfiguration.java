6e2c8869197d0a972b3aab45f1e8aa901881f413
laxika <laxika91@gmail.com>
2020-01-10 18:36:05 +0100

    #97 - Some rationalisation on the queue sessions.

--- loa-service/loa-queue-artemis-service/src/main/java/com/github/loa/queue/artemis/configuration/QueueClientConfiguration.java
+++ loa-service/loa-queue-artemis-service/src/main/java/com/github/loa/queue/artemis/configuration/QueueClientConfiguration.java
@@ -2,8 +2,10 @@ package com.github.loa.queue.artemis.configuration;
 
 import com.github.loa.queue.configuration.QueueConfigurationProperties;
 import lombok.RequiredArgsConstructor;
+import org.apache.activemq.artemis.api.core.ActiveMQException;
 import org.apache.activemq.artemis.api.core.TransportConfiguration;
 import org.apache.activemq.artemis.api.core.client.ActiveMQClient;
+import org.apache.activemq.artemis.api.core.client.ClientSession;
 import org.apache.activemq.artemis.api.core.client.ClientSessionFactory;
 import org.apache.activemq.artemis.api.core.client.ServerLocator;
 import org.apache.activemq.artemis.core.remoting.impl.netty.NettyConnectorFactory;
@@ -20,6 +22,14 @@ import java.util.Map;
 public class QueueClientConfiguration {
 
     private final QueueConfigurationProperties queueConfigurationProperties;
+
+    /*
+     * We create a default session that's necessary to create messages in the serializers/deserializers etc.
+     */
+    @Bean(destroyMethod = "stop")
+    public ClientSession clientSession(final ClientSessionFactory clientSessionFactory) throws ActiveMQException {
+        return clientSessionFactory.createSession();
+    }
 
     @Bean
     public ClientSessionFactory clientSessionFactory(final ServerLocator serverLocator) throws Exception {
