e3d86e10cec4a187eefc4694a731fc3c73278477
laxika <laxika91@gmail.com>
2020-01-10 17:58:29 +0100

    #97 - Consumers also have an executor.

---
+++ loa-service/loa-queue-artemis-service/src/main/java/com/github/loa/queue/artemis/service/consumer/pool/ClientConsumerAllocator.java
@@ -0,0 +1,32 @@
+package com.github.loa.queue.artemis.service.consumer.pool;
+
+import com.github.loa.queue.artemis.service.consumer.pool.domain.PoolableClientConsumer;
+import com.github.loa.queue.service.domain.Queue;
+import com.github.loa.queue.service.domain.QueueException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.activemq.artemis.api.core.ActiveMQException;
+import stormpot.Allocator;
+import stormpot.Slot;
+
+@Slf4j
+@RequiredArgsConstructor
+public class ClientConsumerAllocator implements Allocator<PoolableClientConsumer> {
+
+    private final Queue supportedQueue;
+    private final ClientConsumerFactory clientConsumerFactory;
+
+    @Override
+    public PoolableClientConsumer allocate(final Slot slot) {
+        return new PoolableClientConsumer(slot, clientConsumerFactory.createConsumer(supportedQueue));
+    }
+
+    @Override
+    public void deallocate(final PoolableClientConsumer poolable) {
+        try {
+            poolable.getClientConsumer().close();
+        } catch (final ActiveMQException e) {
+            throw new QueueException("Unable to close client consumer!", e);
+        }
+    }
+}
