72d6aa61d7c4fb2b29f5ab41c1e773884b17830b
Laxika <laxika91@gmail.com>
2020-01-12 22:24:25 +0100

    #158 - Added statistics logging to the Generator Application when using the file source.

--- loa-service/loa-source-file-service/src/main/java/com/github/loa/source/file/service/location/FileDocumentLocationFactory.java
+++ loa-service/loa-source-file-service/src/main/java/com/github/loa/source/file/service/location/FileDocumentLocationFactory.java
@@ -3,7 +3,9 @@ package com.github.loa.source.file.service.location;
 import com.github.loa.source.file.service.FileSourceFactory;
 import com.github.loa.source.service.DocumentLocationFactory;
 import com.github.loa.url.service.URLConverter;
+import io.micrometer.core.instrument.Counter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import reactor.core.publisher.Flux;
 
 import java.io.BufferedReader;
@@ -18,12 +20,16 @@ public class FileDocumentLocationFactory implements DocumentLocationFactory {
     private final URLConverter urlConverter;
     private final Path filePath;
 
+    @Qualifier("processedDocumentLocationCount")
+    private final Counter processedDocumentLocationCount;
+
     @Override
     public Flux<URL> streamLocations() {
         final BufferedReader reader = new BufferedReader(new InputStreamReader(
                 fileSourceFactory.newInputStream(filePath)));
 
         return Flux.fromStream(reader.lines())
+                .doOnNext(line -> processedDocumentLocationCount.increment())
                 .flatMap(urlConverter::convert);
     }
 }
