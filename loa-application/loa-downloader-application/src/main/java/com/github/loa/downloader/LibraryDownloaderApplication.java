531916167ecdfa7fcc539de88da78242cc1b39bc
Laxika <laxika91@gmail.com>
2020-01-12 22:26:12 +0100

    #97 - Removed an unused Slf4j annotation.

--- loa-application/loa-downloader-application/src/main/java/com/github/loa/downloader/LibraryDownloaderApplication.java
+++ loa-application/loa-downloader-application/src/main/java/com/github/loa/downloader/LibraryDownloaderApplication.java
@@ -1,6 +1,5 @@
 package com.github.loa.downloader;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.reactive.function.client.ClientHttpConnectorAutoConfiguration;
@@ -9,7 +8,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 /**
  * The runner class of the downloader application.
  */
-@Slf4j
 @EnableScheduling
 @SpringBootApplication(scanBasePackages = "com.github.loa", exclude = ClientHttpConnectorAutoConfiguration.class)
 public class LibraryDownloaderApplication {
