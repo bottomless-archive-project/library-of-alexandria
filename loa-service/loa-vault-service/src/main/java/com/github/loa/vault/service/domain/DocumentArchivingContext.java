fa6fb681ce6befc3e4470308f88b9c7fbeccf2e8
laxika <laxika91@gmail.com>
2020-01-24 17:21:47 +0100

    #97 - Removed an unused input statement.

--- loa-service/loa-vault-service/src/main/java/com/github/loa/vault/service/domain/DocumentArchivingContext.java
+++ loa-service/loa-vault-service/src/main/java/com/github/loa/vault/service/domain/DocumentArchivingContext.java
@@ -4,8 +4,6 @@ import com.github.loa.document.service.domain.DocumentType;
 import lombok.Builder;
 import lombok.Getter;
 
-import java.nio.file.Path;
-
 @Getter
 @Builder
 public class DocumentArchivingContext {
