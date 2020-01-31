586fd70e77bdba1fcfe4e6bd278738fee9fd49a4
laxika <laxika91@gmail.com>
2020-01-31 18:26:34 +0100

    #159 - Fixed a couple of browser errors.

--- loa-service/loa-queue-artemis-service/src/main/java/com/github/loa/queue/artemis/service/consumer/deserializer/DocumentArchivingMessageDeserializer.java
+++ loa-service/loa-queue-artemis-service/src/main/java/com/github/loa/queue/artemis/service/consumer/deserializer/DocumentArchivingMessageDeserializer.java
@@ -3,11 +3,12 @@ package com.github.loa.queue.artemis.service.consumer.deserializer;
 import com.github.loa.queue.artemis.configuration.QueueServerConfiguration;
 import com.github.loa.queue.service.domain.Queue;
 import com.github.loa.queue.service.domain.message.DocumentArchivingMessage;
-import org.apache.activemq.artemis.api.core.ActiveMQBuffer;
 import org.apache.activemq.artemis.api.core.client.ClientMessage;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.stereotype.Service;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 
 @Service
@@ -23,13 +24,21 @@ public class DocumentArchivingMessageDeserializer implements MessageDeserializer
 
         final InputStream inputStream = new InputStream() {
 
-            private ActiveMQBuffer data;
+            private InputStream data;
             private int dataRead = 0;
 
             @Override
             public int read() {
-                if (data == null || !data.readable()) {
-                    data = clientMessage.getBodyBuffer().readBytes(clientMessage.getBodyBuffer().readableBytes());
+                try {
+                    if (data == null || data.available() == 0) {
+                        final int readableBytes = clientMessage.getBodyBuffer().readableBytes();
+                        final byte[] bytes = new byte[readableBytes];
+
+                        clientMessage.getBodyBuffer().readBytes(bytes);
+                        data = new ByteArrayInputStream(bytes);
+                    }
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
                 }
 
                 // If read everything that was required
@@ -39,7 +48,11 @@ public class DocumentArchivingMessageDeserializer implements MessageDeserializer
 
                 dataRead++;
 
-                return data.readByte();
+                try {
+                    return data.read();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
             }
         };
 
