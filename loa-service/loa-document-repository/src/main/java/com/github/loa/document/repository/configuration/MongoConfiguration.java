6f0d7b9bdc6f3b832558f642057db0068bc6a8bf
Laxika <laxika91@gmail.com>
2020-02-13 21:35:16 +0100

    #172 - Removed the existing mongo configuration.

--- loa-service/loa-document-repository/src/main/java/com/github/loa/document/repository/configuration/MongoConfiguration.java
+++ loa-service/loa-document-repository/src/main/java/com/github/loa/document/repository/configuration/MongoConfiguration.java
@@ -1,21 +1,13 @@
 package com.github.loa.document.repository.configuration;
 
 import com.github.loa.document.repository.domain.DocumentDatabaseEntity;
-import com.mongodb.MongoClientSettings;
 import com.mongodb.client.model.IndexOptions;
 import com.mongodb.client.model.Indexes;
-import com.mongodb.reactivestreams.client.MongoClient;
-import com.mongodb.reactivestreams.client.MongoClients;
 import com.mongodb.reactivestreams.client.MongoCollection;
 import com.mongodb.reactivestreams.client.MongoDatabase;
-import org.bson.codecs.configuration.CodecRegistry;
-import org.bson.codecs.pojo.PojoCodecProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import reactor.core.publisher.Mono;
-
-import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
-import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 
 @Configuration
 public class MongoConfiguration {
@@ -38,17 +30,5 @@ public class MongoConfiguration {
                 "checksum", "fileSize", "type"), uniqueFileIndexOptions)).subscribe();
 
         return documentDatabaseEntityMongoCollection;
-    }
-
-    @Bean
-    public MongoDatabase mongoDatabase() {
-        CodecRegistry pojoCodecRegistry = fromRegistries(MongoClientSettings.getDefaultCodecRegistry(),
-                fromProviders(PojoCodecProvider.builder().automatic(true).build()));
-        MongoClientSettings settings = MongoClientSettings.builder()
-                .codecRegistry(pojoCodecRegistry)
-                .build();
-        MongoClient mongoClient = MongoClients.create(settings);
-
-        return mongoClient.getDatabase("loa");
     }
 }
