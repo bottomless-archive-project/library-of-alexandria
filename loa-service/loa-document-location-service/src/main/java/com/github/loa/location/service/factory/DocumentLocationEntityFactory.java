5acdfeaee514542599589aebc2c338ff90794fb1
Laxika <laxika91@gmail.com>
2020-02-13 17:08:16 +0100

    #172 - Minor improvements in the vault's database connections.

--- loa-service/loa-document-location-service/src/main/java/com/github/loa/location/service/factory/DocumentLocationEntityFactory.java
+++ loa-service/loa-document-location-service/src/main/java/com/github/loa/location/service/factory/DocumentLocationEntityFactory.java
@@ -4,6 +4,8 @@ import com.github.loa.location.repository.DocumentLocationRepository;
 import com.github.loa.location.repository.domain.DocumentLocationDatabaseEntity;
 import com.github.loa.location.service.factory.domain.DocumentLocationCreationContext;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
@@ -16,11 +18,20 @@ public class DocumentLocationEntityFactory {
     public Mono<Boolean> isDocumentLocationExistsOrCreate(
             final DocumentLocationCreationContext documentLocationCreationContext) {
         final DocumentLocationDatabaseEntity documentLocationDatabaseEntity = new DocumentLocationDatabaseEntity();
-        documentLocationDatabaseEntity.setId(documentLocationCreationContext.getId());
+        documentLocationDatabaseEntity.setId(decodeId(documentLocationCreationContext.getId()));
         documentLocationDatabaseEntity.setSource(documentLocationCreationContext.getSource());
         documentLocationDatabaseEntity.setUrl(documentLocationCreationContext.getUrl());
         documentLocationDatabaseEntity.setDownloaderVersion(documentLocationCreationContext.getDownloaderVersion());
 
         return documentLocationRepository.existsOrInsert(documentLocationDatabaseEntity);
+    }
+
+    //TODO: Move this to a helper!
+    private byte[] decodeId(final String id) {
+        try {
+            return Hex.decodeHex(id);
+        } catch (final DecoderException e) {
+            throw new RuntimeException("Unable to decode id: " + id + "!", e);
+        }
     }
 }
--- loa-service/loa-document-repository/src/main/java/com/github/loa/document/repository/configuration/MongoConfiguration.java
+++ loa-service/loa-document-repository/src/main/java/com/github/loa/document/repository/configuration/MongoConfiguration.java
@@ -1,5 +1,11 @@
 package com.github.loa.document.repository.configuration;
 
+import com.mongodb.MongoClientSettings;
+import com.mongodb.reactivestreams.client.MongoClient;
+import com.mongodb.reactivestreams.client.MongoClients;
+import com.mongodb.reactivestreams.client.MongoDatabase;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDbFactory;
@@ -8,8 +14,23 @@ import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
 import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
 import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 
+import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
+import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
+
 @Configuration
 public class MongoConfiguration {
+
+    @Bean
+    public MongoDatabase mongoDatabase() {
+        CodecRegistry pojoCodecRegistry = fromRegistries(MongoClientSettings.getDefaultCodecRegistry(),
+                fromProviders(PojoCodecProvider.builder().automatic(true).build()));
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .codecRegistry(pojoCodecRegistry)
+                .build();
+        MongoClient mongoClient = MongoClients.create(settings);
+
+        return mongoClient.getDatabase("loa");
+    }
 
     @Bean
     public MappingMongoConverter mappingMongoConverter(final MongoDbFactory mongoDbFactory, final MongoMappingContext context) {
