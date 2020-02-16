ef769b1458d347946531b548100d09165fed87e6
laxika <laxika91@gmail.com>
2020-02-16 11:37:52 +0100

    #97 - Javadocs.

--- loa-application/loa-vault-application/src/main/java/com/github/loa/vault/LibraryVaultApplication.java
+++ loa-application/loa-vault-application/src/main/java/com/github/loa/vault/LibraryVaultApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * This application is responsible for storing and retrieving the documents.
+ *
+ * The main logic of the archiving is found in the {@link com.github.loa.vault.service.listener.VaultQueueListener}.
  */
 @SpringBootApplication(scanBasePackages = "com.github.loa")
 public class LibraryVaultApplication {
