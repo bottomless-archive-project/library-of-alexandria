1b9f2fa20a4dda9d08a7729b88bcb7afa51cfbff
laxika <laxika91@gmail.com>
2020-02-28 16:44:38 +0100

    #31 - Added the functionality to skip lines while reading document locations from a file.

--- loa-service/loa-source-file-service/src/main/java/com/github/loa/source/file/configuration/FileDocumentSourceConfigurationProperties.java
+++ loa-service/loa-source-file-service/src/main/java/com/github/loa/source/file/configuration/FileDocumentSourceConfigurationProperties.java
@@ -5,11 +5,29 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+/**
+ * Contains the configurations that could be set from the property files regarding the file based document location
+ * source processing.
+ */
 @Data
 @Component
 @ConfigurationProperties("loa.source.file")
 public class FileDocumentSourceConfigurationProperties {
 
+    /**
+     * The location of the input file on the disc.
+     */
     private String location;
+
+    /**
+     * The encoding of the file.
+     *
+     * @see FileEncodingType
+     */
     private FileEncodingType encoding;
+
+    /**
+     * How many lines should be skipped before starting to process locations again.
+     */
+    private int skipLines;
 }
--- loa-service/loa-source-file-service/src/main/java/com/github/loa/source/file/service/location/FileDocumentLocationFactory.java
+++ loa-service/loa-source-file-service/src/main/java/com/github/loa/source/file/service/location/FileDocumentLocationFactory.java
@@ -1,10 +1,12 @@
 package com.github.loa.source.file.service.location;
 
+import com.github.loa.source.file.configuration.FileDocumentSourceConfigurationProperties;
 import com.github.loa.source.file.service.FileSourceFactory;
 import com.github.loa.source.service.DocumentLocationFactory;
 import com.github.loa.url.service.URLConverter;
 import io.micrometer.core.instrument.Counter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import reactor.core.publisher.Flux;
 
@@ -13,9 +15,11 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.nio.file.Path;
 
+@Slf4j
 @RequiredArgsConstructor
 public class FileDocumentLocationFactory implements DocumentLocationFactory {
 
+    private final FileDocumentSourceConfigurationProperties fileDocumentSourceConfigurationProperties;
     private final FileSourceFactory fileSourceFactory;
     private final URLConverter urlConverter;
     private final Path filePath;
@@ -25,11 +29,21 @@ public class FileDocumentLocationFactory implements DocumentLocationFactory {
 
     @Override
     public Flux<URL> streamLocations() {
+        if (fileDocumentSourceConfigurationProperties.getSkipLines() > 0) {
+            log.info("Skipping the first " + fileDocumentSourceConfigurationProperties.getSkipLines() + " lines.");
+        }
+
+        if (fileDocumentSourceConfigurationProperties.getSkipLines() < 0) {
+            throw new RuntimeException("Skip lines shouldn't be negative! It is set to "
+                    + fileDocumentSourceConfigurationProperties.getSkipLines() + " lines.");
+        }
+
         final BufferedReader reader = new BufferedReader(new InputStreamReader(
                 fileSourceFactory.newInputStream(filePath)));
 
         return Flux.fromStream(reader.lines())
                 .doOnNext(line -> processedDocumentLocationCount.increment())
+                .skip(fileDocumentSourceConfigurationProperties.getSkipLines())
                 .flatMap(urlConverter::convert);
     }
 }
