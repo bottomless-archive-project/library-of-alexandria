114d91b8ef637f9c969a98ab7481734787aff90d
Laxika <laxika91@gmail.com>
2020-03-21 23:22:53 +0100

    #187 - Disabled some logging that's not really necessary.

--- loa-application/loa-downloader-application/src/main/java/com/github/loa/downloader/LibraryDownloaderApplication.java
+++ loa-application/loa-downloader-application/src/main/java/com/github/loa/downloader/LibraryDownloaderApplication.java
@@ -1,9 +1,13 @@
 package com.github.loa.downloader;
 
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.reactive.function.client.ClientHttpConnectorAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.Arrays;
 
 /**
  * The runner class of the downloader application.
@@ -12,7 +16,23 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication(scanBasePackages = "com.github.loa", exclude = ClientHttpConnectorAutoConfiguration.class)
 public class LibraryDownloaderApplication {
 
+    private static final String[] UNUSABLE_LOGGING = new String[]{
+            "org.apache.pdfbox.util.PDFStreamEngine",
+            "org.apache.pdfbox.pdmodel.font.PDSimpleFont",
+            "org.apache.pdfbox.pdmodel.font.PDFont",
+            "org.apache.pdfbox.pdmodel.font.FontManager",
+            "org.apache.pdfbox.pdfparser.PDFObjectStreamParser",
+            "org.apache.fontbox.ttf.GlyphSubstitutionTable"
+    };
+
     public static void main(final String[] args) {
+        disableUnusableLogging();
+
         SpringApplication.run(LibraryDownloaderApplication.class, args);
+    }
+
+    private static void disableUnusableLogging() {
+        Arrays.stream(UNUSABLE_LOGGING)
+                .forEach(logger -> LogManager.getLogger(logger).isEnabled(Level.OFF));
     }
 }
