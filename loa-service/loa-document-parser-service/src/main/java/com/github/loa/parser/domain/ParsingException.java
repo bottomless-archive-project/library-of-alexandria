594821ddf4f0ad4f846f2e90f2060736210da851
Laxika <laxika91@gmail.com>
2020-03-23 22:32:21 +0100

    #188 - Working on a fix for the parser to disable the parsing of invalid documents.

---
+++ loa-service/loa-document-parser-service/src/main/java/com/github/loa/parser/domain/ParsingException.java
@@ -0,0 +1,8 @@
+package com.github.loa.parser.domain;
+
+public class ParsingException extends RuntimeException {
+
+    public ParsingException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
+}
