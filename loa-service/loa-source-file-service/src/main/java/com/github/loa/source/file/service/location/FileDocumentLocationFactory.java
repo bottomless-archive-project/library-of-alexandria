147861add6821e2c30431c258b87a7b2c36ba803
Laxika <laxika91@gmail.com>
2020-04-24 17:06:20 +0200

    #188 - Deleted an accidentaly added skip.

--- loa-service/loa-source-file-service/src/main/java/com/github/loa/source/file/service/location/FileDocumentLocationFactory.java
+++ loa-service/loa-source-file-service/src/main/java/com/github/loa/source/file/service/location/FileDocumentLocationFactory.java
@@ -30,7 +30,6 @@ public class FileDocumentLocationFactory implements DocumentLocationFactory {
 
         return Flux.fromStream(reader.lines())
                 .doOnNext(line -> processedDocumentLocationCount.increment())
-                .skip(2147483647)
                 .flatMap(urlConverter::convert);
     }
 }
