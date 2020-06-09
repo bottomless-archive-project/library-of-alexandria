87faf80ac56b2b0c3d9ed1daeb07cb29c6a495ce
laxika <laxika91@gmail.com>
2020-06-09 19:06:56 +0200

    #41 - Added the newly required configuration properties.

--- loa-service/loa-vault-service/src/main/java/com/github/loa/vault/configuration/VaultConfigurationProperties.java
+++ loa-service/loa-vault-service/src/main/java/com/github/loa/vault/configuration/VaultConfigurationProperties.java
@@ -4,10 +4,29 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+/**
+ * Holds the configuration properties to the vault.
+ */
 @Data
 @Component
 @ConfigurationProperties("loa.vault")
 public class VaultConfigurationProperties {
 
+    /**
+     * The unique name of the vault. This name will be saved when archiving new documents and will be used to identify
+     * the vault that holds a given document. Do not change this after creating the vault because the documents
+     * previously archived by this vault are not going to be accessible.
+     */
+    private String name;
+
+    /**
+     * If this vault should archive new documents.
+     */
+    private boolean archiving;
+
+    /**
+     * This version number will be saved to the database for every document that has been archived. It will be used
+     * later on if its necessary to run cleanup or fixing tasks that are specific to a given version.
+     */
     private int versionNumber;
 }
