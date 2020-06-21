9dfe5fd9c17f1c38086060610ebc74101d15cdf8
laxika <laxika91@gmail.com>
2020-06-21 19:18:04 +0200

    #201 - Added the empty responses and ordered the types and statuses in the dashboard response.

--- loa-application/loa-web-application/src/main/java/com/github/loa/web/view/document/service/DocumentStatisticsResponseFactory.java
+++ loa-application/loa-web-application/src/main/java/com/github/loa/web/view/document/service/DocumentStatisticsResponseFactory.java
@@ -1,10 +1,16 @@
 package com.github.loa.web.view.document.service;
 
+import com.github.loa.document.service.domain.DocumentStatus;
+import com.github.loa.document.service.domain.DocumentType;
 import com.github.loa.document.service.entity.factory.DocumentEntityFactory;
 import com.github.loa.web.view.document.response.DashboardDocumentStatisticsResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+
+import java.util.Arrays;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -25,12 +31,16 @@ public class DocumentStatisticsResponseFactory {
     private Mono<DashboardDocumentStatisticsResponse.DashboardDocumentStatisticsResponseBuilder> fillDocumentCountByType(
             final DashboardDocumentStatisticsResponse.DashboardDocumentStatisticsResponseBuilder builder) {
         return documentEntityFactory.getCountByType()
+                .map(typeMap -> Arrays.stream(DocumentType.values())
+                        .collect(Collectors.toMap(documentType -> documentType, documentType -> typeMap.getOrDefault(documentType, 0), (a, b) -> b, TreeMap::new)))
                 .map(builder::documentCountByType);
     }
 
     private Mono<DashboardDocumentStatisticsResponse.DashboardDocumentStatisticsResponseBuilder> fillDocumentCountByStatus(
             final DashboardDocumentStatisticsResponse.DashboardDocumentStatisticsResponseBuilder builder) {
         return documentEntityFactory.getCountByStatus()
+                .map(statusMap -> Arrays.stream(DocumentStatus.values())
+                        .collect(Collectors.toMap(documentStatus -> documentStatus, documentStatus -> statusMap.getOrDefault(documentStatus, 0), (a, b) -> b, TreeMap::new)))
                 .map(builder::documentCountByStatus);
     }
 }
