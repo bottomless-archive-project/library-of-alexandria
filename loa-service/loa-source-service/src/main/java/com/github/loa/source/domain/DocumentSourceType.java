2eb1a3d21a1d7e5a40c6ae7d3d7fbb956df05ea4
laxika <laxika91@gmail.com>
2020-07-24 00:42:41 +0200

    #210 - Added javadocs.

--- loa-service/loa-source-service/src/main/java/com/github/loa/source/domain/DocumentSourceType.java
+++ loa-service/loa-source-service/src/main/java/com/github/loa/source/domain/DocumentSourceType.java
@@ -1,9 +1,12 @@
 package com.github.loa.source.domain;
 
+/**
+ * Describe the various type of sources where document locations could be gathered.
+ */
 public enum DocumentSourceType {
 
     /**
-     * The source data (URLs) are loaded from a file.
+     * The source data are loaded from a file.
      */
     FILE,
 
--- loa-service/loa-source-service/src/main/java/com/github/loa/source/service/DocumentLocationFactory.java
+++ loa-service/loa-source-service/src/main/java/com/github/loa/source/service/DocumentLocationFactory.java
@@ -4,7 +4,15 @@ import reactor.core.publisher.Flux;
 
 import java.net.URL;
 
+/**
+ * This class is streaming new URLs that should be checked for new documents.
+ */
 public interface DocumentLocationFactory {
 
+    /**
+     * This method is streaming new URLs that should be checked for new documents.
+     *
+     * @return stream of URLs that should be checked for new documents
+     */
     Flux<URL> streamLocations();
 }
