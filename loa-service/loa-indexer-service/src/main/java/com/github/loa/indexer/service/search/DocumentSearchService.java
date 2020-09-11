fccf3078cde40dfea34c7e23aaf3dbe7b5d6e5b2
laxika <laxika91@gmail.com>
2020-09-12 00:54:47 +0200

    #221 - Fixed a deprecated method call in the DocumentSearchService.

--- loa-service/loa-indexer-service/src/main/java/com/github/loa/indexer/service/search/DocumentSearchService.java
+++ loa-service/loa-indexer-service/src/main/java/com/github/loa/indexer/service/search/DocumentSearchService.java
@@ -3,7 +3,7 @@ package com.github.loa.indexer.service.search;
 import com.github.loa.indexer.domain.DocumentSearchResult;
 import com.github.loa.indexer.domain.IndexerAccessException;
 import com.github.loa.indexer.service.search.domain.SearchContext;
-import com.github.loa.indexer.service.search.request.SearchRequestFactory;
+import com.github.loa.indexer.service.search.request.IndexerRequestFactory;
 import com.github.loa.indexer.service.search.transformer.DocumentSearchEntityTransformer;
 import lombok.RequiredArgsConstructor;
 import org.elasticsearch.action.search.SearchRequest;
@@ -11,8 +11,6 @@ import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.client.core.CountRequest;
-import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -22,11 +20,11 @@ import java.io.IOException;
 public class DocumentSearchService {
 
     private final RestHighLevelClient restHighLevelClient;
-    private final SearchRequestFactory searchRequestFactory;
+    private final IndexerRequestFactory indexerRequestFactory;
     private final DocumentSearchEntityTransformer documentSearchEntityTransformer;
 
     public DocumentSearchResult searchDocuments(final SearchContext searchContext) {
-        final SearchRequest searchRequest = searchRequestFactory.newKeywordSearchRequest(searchContext);
+        final SearchRequest searchRequest = indexerRequestFactory.newKeywordSearchRequest(searchContext);
 
         try {
             final SearchResponse searchResponse = restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
@@ -35,18 +33,17 @@ public class DocumentSearchService {
                     .searchHits(documentSearchEntityTransformer.transform(searchResponse.getHits()))
                     .totalHitCount(searchResponse.getHits().getTotalHits().value)
                     .build();
-        } catch (IOException e) {
+        } catch (final IOException e) {
             throw new IndexerAccessException("Failed to search in the indexer!", e);
         }
     }
 
     public long countDocuments() {
-        final CountRequest countRequest = new CountRequest("vault_documents")
-                .source(SearchSourceBuilder.searchSource().query(QueryBuilders.matchAllQuery()));
+        final CountRequest countRequest = indexerRequestFactory.newCountDocumentsRequest();
 
         try {
             return restHighLevelClient.count(countRequest, RequestOptions.DEFAULT).getCount();
-        } catch (IOException e) {
+        } catch (final IOException e) {
             throw new IndexerAccessException("Failed to search in the indexer!", e);
         }
     }
--- loa-service/loa-indexer-service/src/main/java/com/github/loa/indexer/service/search/request/IndexerRequestFactory.java
+++ loa-service/loa-indexer-service/src/main/java/com/github/loa/indexer/service/search/request/IndexerRequestFactory.java
@@ -4,18 +4,25 @@ import com.github.loa.indexer.service.search.domain.SearchContext;
 import com.github.loa.indexer.service.search.domain.SearchField;
 import lombok.RequiredArgsConstructor;
 import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.client.core.CountRequest;
 import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class SearchRequestFactory {
+public class IndexerRequestFactory {
 
     private final QueryBuilderFactory queryBuilderFactory;
 
     private static final String[] INDICES = {"vault_documents"};
+
+    public CountRequest newCountDocumentsRequest() {
+        return new CountRequest(INDICES)
+                .query(QueryBuilders.matchAllQuery());
+    }
 
     public SearchRequest newKeywordSearchRequest(final SearchContext searchContext) {
         return new SearchRequest(INDICES, newDocumentContentQuery(searchContext));
