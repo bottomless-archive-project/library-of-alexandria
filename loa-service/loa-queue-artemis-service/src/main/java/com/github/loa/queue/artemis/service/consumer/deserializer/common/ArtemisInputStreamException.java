371b653bb4151b7e1bf6ee0b9bd080bf8f3963c6
laxika <laxika91@gmail.com>
2021-03-04 01:56:57 +0100

    #268 - Removed two unused classes.

--- loa-service/loa-queue-artemis-service/src/main/java/com/github/loa/queue/artemis/service/consumer/deserializer/common/ArtemisInputStreamException.java
+++
@@ -1,8 +0,0 @@
-package com.github.loa.queue.artemis.service.consumer.deserializer.common;
-
-public class ArtemisInputStreamException extends RuntimeException {
-
-    public ArtemisInputStreamException(final Throwable throwable) {
-        super(throwable);
-    }
-}
