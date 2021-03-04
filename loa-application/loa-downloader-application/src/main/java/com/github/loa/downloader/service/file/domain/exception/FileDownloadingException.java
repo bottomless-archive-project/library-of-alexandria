9f4637c84e4a8554a7dede5c31c5e3751830ce13
laxika <laxika91@gmail.com>
2021-03-04 01:57:58 +0100

    #268 - Removed more unused classes.

--- loa-application/loa-downloader-application/src/main/java/com/github/loa/downloader/service/file/domain/exception/FileDownloadingException.java
+++
@@ -1,8 +0,0 @@
-package com.github.loa.downloader.service.file.domain.exception;
-
-public class FileDownloadingException extends RuntimeException {
-
-    public FileDownloadingException(final Throwable throwable) {
-        super(throwable);
-    }
-}
