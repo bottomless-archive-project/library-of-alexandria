ddaf4e6727c96e33e4338db51e678be3f0e3e14e
laxika <laxika91@gmail.com>
2021-03-04 01:12:57 +0100

    #267 - The Vault Application uses a generator, instead of a random parallel mono to read the queue.

--- loa-application/loa-vault-application/src/main/java/com/github/loa/vault/service/listener/VaultQueueListener.java
+++ loa-application/loa-vault-application/src/main/java/com/github/loa/vault/service/listener/VaultQueueListener.java
@@ -2,7 +2,6 @@ package com.github.loa.vault.service.listener;
 
 import com.github.loa.queue.service.QueueManipulator;
 import com.github.loa.queue.service.domain.Queue;
-import com.github.loa.queue.service.domain.message.DocumentArchivingMessage;
 import com.github.loa.vault.service.transformer.DocumentArchivingContextTransformer;
 import com.github.loa.vault.service.VaultDocumentManager;
 import lombok.RequiredArgsConstructor;
@@ -10,7 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
-import reactor.core.publisher.Mono;
+import reactor.core.publisher.Flux;
 import reactor.core.scheduler.Schedulers;
 
 @Slf4j
@@ -21,6 +20,7 @@ public class VaultQueueListener implements CommandLineRunner {
 
     private final QueueManipulator queueManipulator;
     private final VaultDocumentManager vaultDocumentManager;
+    private final VaultQueueConsumer vaultQueueConsumer;
     private final DocumentArchivingContextTransformer documentArchivingContextTransformer;
 
     @Override
@@ -29,17 +29,10 @@ public class VaultQueueListener implements CommandLineRunner {
         log.info("Initialized queue processing! There are {} messages available in the archiving queue!",
                 queueManipulator.getMessageCount(Queue.DOCUMENT_ARCHIVING_QUEUE));
 
-        Mono.just(1)
-                .repeat()
-                .parallel()
-                .runOn(Schedulers.boundedElastic())
-                .map(random -> readDocumentArchivingMessage())
+        Flux.generate(vaultQueueConsumer)
+                .publishOn(Schedulers.boundedElastic())
                 .map(documentArchivingContextTransformer::transform)
                 .flatMap(vaultDocumentManager::archiveDocument)
                 .subscribe();
-    }
-
-    private DocumentArchivingMessage readDocumentArchivingMessage() {
-        return queueManipulator.readMessage(Queue.DOCUMENT_ARCHIVING_QUEUE, DocumentArchivingMessage.class);
     }
 }
