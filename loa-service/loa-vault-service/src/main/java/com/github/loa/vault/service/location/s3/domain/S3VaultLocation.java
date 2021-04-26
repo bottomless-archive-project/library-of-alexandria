331cd54c48ec8bc37383a57987cabe4fe34636e1
laxika <laxika91@gmail.com>
2021-04-27 00:39:21 +0200

    #271 - Fixed a checkstyle error in S3VaultLocation.

--- loa-service/loa-vault-service/src/main/java/com/github/loa/vault/service/location/s3/domain/S3VaultLocation.java
+++ loa-service/loa-vault-service/src/main/java/com/github/loa/vault/service/location/s3/domain/S3VaultLocation.java
@@ -7,7 +7,12 @@ import lombok.ToString;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.core.sync.ResponseTransformer;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.*;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.HeadObjectRequest;
+import software.amazon.awssdk.services.s3.model.NoSuchKeyException;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.StorageClass;
 
 import java.io.InputStream;
 import java.util.Optional;
