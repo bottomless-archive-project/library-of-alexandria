35fb22fcdac41f33d51b87624199afcb609e6437
Laxika <laxika91@gmail.com>
2021-05-04 01:04:27 +0200

    #288 - Added the base for search and dashboard.

--- loa-frontend/src/app/app.module.ts
+++ loa-frontend/src/app/app.module.ts
@@ -4,21 +4,27 @@ import {Routes, RouterModule} from '@angular/router';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HomeComponent} from './home/home.component';
+import {SearchComponent} from './search/search.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
+  {path: 'search', component: SearchComponent},
+  {path: 'dashboard', component: DashboardComponent}
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    SearchComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(routes, { useHash: true })
+    RouterModule.forRoot(routes, {useHash: true})
   ],
   exports: [RouterModule],
   providers: [],
