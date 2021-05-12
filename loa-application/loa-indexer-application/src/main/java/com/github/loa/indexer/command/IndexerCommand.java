e5b79bb0737160a87697e70c7e0e3024384f8c3e
Laxika <laxika91@gmail.com>
2021-05-12 19:29:34 +0200

    #293 - Hopefully the final buffer leak fix.

--- loa-application/loa-indexer-application/src/main/java/com/github/loa/indexer/command/IndexerCommand.java
+++ loa-application/loa-indexer-application/src/main/java/com/github/loa/indexer/command/IndexerCommand.java
@@ -11,15 +11,16 @@ import com.github.loa.parser.service.DocumentDataParser;
 import com.github.loa.vault.client.service.VaultClientService;
 import com.github.loa.vault.client.service.domain.VaultAccessException;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.io.buffer.DataBufferUtils;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.SequenceInputStream;
 import java.net.ConnectException;
 
 @Slf4j
@@ -50,24 +51,24 @@ public class IndexerCommand implements CommandLineRunner {
                 .subscribe();
     }
 
+    @SneakyThrows
     private Mono<Void> processDocument(final DocumentEntity documentEntity) {
         return vaultClientService.queryDocument(documentEntity)
-                .reduce(InputStream.nullInputStream(),
-                        (s, d) -> new SequenceInputStream(s, d.asInputStream()))
+                .reduce(new ByteArrayOutputStream(), (boi, value) -> {
+                    try {
+                        boi.write(value.asInputStream().readAllBytes());
+
+                        DataBufferUtils.release(value);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                    return boi;
+                })
+                .map(ByteArrayOutputStream::toByteArray)
                 .publishOn(Schedulers.parallel())
-                .flatMap(documentContent ->
-                        Mono.using(
-                                () -> documentContent,
-                                (content) -> Mono.just(documentDataParser.parseDocumentMetadata(
-                                        documentEntity.getId(), documentEntity.getType(), content)),
-                                (content) -> {
-                                    try {
-                                        content.close();
-                                    } catch (IOException e) {
-                                        log.error("Failed to close vault response!", e);
-                                    }
-                                })
-                )
+                .map(documentContent -> documentDataParser.parseDocumentMetadata(
+                        documentEntity.getId(), documentEntity.getType(), documentContent))
                 .onErrorContinue((throwable, document) -> {
                     //TODO: This should be handled in the VaultClientService but we are unable to do so
                     // because of this onErrorContinue here (no sufficient operator in the reactive toolset).
