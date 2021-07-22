37ecf446e98c15c3ae7835a433e7daf1f4bf0352
laxika <laxika91@gmail.com>
2021-07-22 18:43:37 +0200

    #285 - Added global spinners on transition.

--- loa-frontend/src/app/view/app/app.component.ts
+++ loa-frontend/src/app/view/app/app.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import {
+  Event,
+  NavigationCancel,
+  NavigationEnd,
+  NavigationError,
+  NavigationStart,
+  Router
+} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +15,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'loa-frontend';
+  loading = false;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((event: Event) => {
+      console.log('New route change event!', event);
+
+      switch (true) {
+        case event instanceof NavigationStart: {
+          this.loading = true;
+          break;
+        }
+
+        case event instanceof NavigationEnd:
+        case event instanceof NavigationCancel:
+        case event instanceof NavigationError: {
+          this.loading = false;
+          break;
+        }
+        default: {
+          break;
+        }
+      }
+    });
+  }
 }
