bb68661aa238a6c9adbf7f8daabb0bab47cb4aaf
laxika <laxika91@gmail.com>
2021-08-23 17:31:12 +0200

    #302 - Fixed a bug that was caused by the new mongodb driver/server version.

--- loa-service/loa-document-location-repository/src/main/java/com/github/loa/location/repository/DocumentLocationRepository.java
+++ loa-service/loa-document-location-repository/src/main/java/com/github/loa/location/repository/DocumentLocationRepository.java
@@ -11,16 +11,22 @@ import reactor.core.publisher.Mono;
 @RequiredArgsConstructor
 public class DocumentLocationRepository {
 
+    private static final int DUPLICATE_DOCUMENT_ID_ERROR_CODE = 11000;
+
     private final MongoCollection<DocumentLocationDatabaseEntity> documentLocationDatabaseEntityMongoCollection;
 
     public Mono<Boolean> existsOrInsert(final DocumentLocationDatabaseEntity documentLocationDatabaseEntity) {
         return Mono.from(documentLocationDatabaseEntityMongoCollection.insertOne(documentLocationDatabaseEntity))
                 .map(result -> Boolean.FALSE)
-                .onErrorReturn(throwable -> throwable instanceof MongoWriteException
-                        && throwable.getMessage().startsWith("E11000 duplicate key error"), Boolean.TRUE);
+                .onErrorReturn(this::isDuplicateDocumentIdError, Boolean.TRUE);
     }
 
     public Mono<Long> count() {
         return Mono.from(documentLocationDatabaseEntityMongoCollection.countDocuments());
+    }
+
+    private boolean isDuplicateDocumentIdError(final Throwable throwable) {
+        return throwable instanceof MongoWriteException mongoWriteException
+                && mongoWriteException.getError().getCode() == DUPLICATE_DOCUMENT_ID_ERROR_CODE;
     }
 }
