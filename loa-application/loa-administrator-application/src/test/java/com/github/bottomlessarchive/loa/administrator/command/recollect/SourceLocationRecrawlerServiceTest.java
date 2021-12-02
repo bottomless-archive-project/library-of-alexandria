612c381467f73b8b6b9af63a4376d4825d035b9a
laxika <laxika91@gmail.com>
2021-12-02 22:25:15 +0100

    #338 - Added a failing tests for the upcoming request sending.

---
+++ loa-application/loa-administrator-application/src/test/java/com/github/bottomlessarchive/loa/administrator/command/recollect/SourceLocationRecrawlerServiceTest.java
@@ -0,0 +1,73 @@
+package com.github.bottomlessarchive.loa.administrator.command.recollect;
+
+import com.github.bottomlessarchive.loa.document.service.domain.DocumentEntity;
+import com.github.bottomlessarchive.loa.document.service.domain.DocumentType;
+import com.github.bottomlessarchive.loa.stage.service.StageLocationFactory;
+import com.github.bottomlessarchive.loa.stage.service.domain.StageLocation;
+import com.github.bottomlessarchive.loa.url.service.downloader.FileDownloadManager;
+import com.github.bottomlessarchive.loa.validator.service.DocumentFileValidator;
+import com.github.bottomlessarchive.loa.vault.client.service.VaultClientService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.io.ByteArrayInputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class SourceLocationRecrawlerServiceTest {
+
+    @Mock
+    private StageLocationFactory stageLocationFactory;
+
+    @Mock
+    private DocumentFileValidator documentFileValidator;
+
+    @Mock
+    private VaultClientService vaultClientService;
+
+    @Mock
+    private FileDownloadManager fileDownloadManager;
+
+    @InjectMocks
+    private SourceLocationRecrawlerService underTest;
+
+    @Test
+    void testRecrawlSourceLocationWhenSourceIsDownloadabe() throws MalformedURLException {
+        final URL sourceLocation = new URL("http://example.com/");
+        final DocumentEntity documentEntity = DocumentEntity.builder()
+                .type(DocumentType.PDF)
+                .build();
+
+        final StageLocation stageLocation = mock(StageLocation.class);
+        when(stageLocation.exists())
+                .thenReturn(Mono.just(true));
+        when(stageLocationFactory.getLocation(any(), eq(DocumentType.PDF)))
+                .thenReturn(Mono.just(stageLocation));
+
+        when(documentFileValidator.isValidDocument(any(), eq(DocumentType.PDF)))
+                .thenReturn(Mono.just(true));
+
+        when(stageLocation.openStream())
+                .thenReturn(new ByteArrayInputStream(new byte[]{1, 2, 3}));
+        when(vaultClientService.replaceCorruptDocument(documentEntity, new byte[]{1, 2, 3}))
+                .thenReturn(Mono.empty());
+
+        Mono<DocumentEntity> result = underTest.recrawlSourceLocation(sourceLocation, documentEntity);
+
+        StepVerifier.create(result)
+                .consumeNextWith(localDocumentEntity -> Assertions.assertEquals(documentEntity, localDocumentEntity))
+                .verifyComplete();
+
+        verify(stageLocation).cleanup();
+    }
+}
