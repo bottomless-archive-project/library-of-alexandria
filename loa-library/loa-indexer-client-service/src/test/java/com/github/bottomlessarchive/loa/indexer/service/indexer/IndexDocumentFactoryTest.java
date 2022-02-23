980166d5d2995867c03e7bbfdaf7b79ab56e6296
laxika <laxika91@gmail.com>
2022-02-23 17:53:55 +0100

    #372 - Added test for IndexDocumentFactory.

---
+++ loa-library/loa-indexer-client-service/src/test/java/com/github/bottomlessarchive/loa/indexer/service/indexer/IndexDocumentFactoryTest.java
@@ -0,0 +1,72 @@
+package com.github.bottomlessarchive.loa.indexer.service.indexer;
+
+import com.github.bottomlessarchive.loa.document.service.domain.DocumentType;
+import com.github.bottomlessarchive.loa.indexer.service.indexer.domain.IndexingContext;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class IndexDocumentFactoryTest {
+
+    private IndexDocumentFactory indexDocumentFactory;
+
+    @BeforeEach
+    void setup() {
+        indexDocumentFactory = new IndexDocumentFactory();
+    }
+
+    @Test
+    void testThatOptionalParametersWereNotAddedWhenTheyAreNull() {
+        final Map<String, Object> result = indexDocumentFactory.buildIndexDocument(
+                IndexingContext.builder()
+                        .content("")
+                        .build()
+        );
+
+        assertThat(result)
+                .doesNotContainKeys("title", "author", "date", "language");
+    }
+
+    @Test
+    void testThatOptionalParametersWereNotAddedWhenTheyAreBlank() {
+        final Map<String, Object> result = indexDocumentFactory.buildIndexDocument(
+                IndexingContext.builder()
+                        .content("")
+                        .title("  ")
+                        .author("   ")
+                        .date("   ")
+                        .language("  ")
+                        .build()
+        );
+
+        assertThat(result)
+                .doesNotContainKeys("title", "author", "date", "language");
+    }
+
+    @Test
+    void testParametersAreSanitizedAndAddedCorrectly() {
+        final Map<String, Object> result = indexDocumentFactory.buildIndexDocument(
+                IndexingContext.builder()
+                        .content(" test-content ")
+                        .title(" test-title ")
+                        .author(" test-author ")
+                        .date(" test-date ")
+                        .language(" test-language ")
+                        .pageCount(10)
+                        .type(DocumentType.DOC)
+                        .build()
+        );
+
+        assertThat(result)
+                .containsEntry("content", "test-content")
+                .containsEntry("title", "test-title")
+                .containsEntry("author", "test-author")
+                .containsEntry("date", "test-date")
+                .containsEntry("language", "test-language")
+                .containsEntry("page_count", 10)
+                .containsEntry("type", DocumentType.DOC);
+    }
+}
