2f7cf7e6c73543f92733bfd71dea803ad6d0568c
laxika <laxika91@gmail.com>
2022-03-31 11:34:53 +0200

    #373 - Renamed a test.

--- loa-library/loa-source-file-service/src/test/java/com/github/bottomlessarchive/loa/source/file/service/location/FileDocumentLocationSourceTest.java
+++ loa-library/loa-source-file-service/src/test/java/com/github/bottomlessarchive/loa/source/file/service/location/FileDocumentLocationSourceTest.java
@@ -26,7 +26,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-class FileDocumentLocationFactoryTest {
+class FileDocumentLocationSourceTest {
 
     @Mock
     private FileDocumentSourceConfigurationProperties fileDocumentSourceConfigurationProperties;
