5dcc45c59fbe85db2161d9f81420f3324f8dce6a
laxika <laxika91@gmail.com>
2022-06-24 21:07:02 +0200

    #369 - Changed to webflux in the Staging Application because it supports zero-copy.

--- loa-application/loa-staging-application/src/main/java/com/github/bottomlessarchive/loa/stage/view/document/controller/StageDocumentController.java
+++ loa-application/loa-staging-application/src/main/java/com/github/bottomlessarchive/loa/stage/view/document/controller/StageDocumentController.java
@@ -1,17 +1,20 @@
 package com.github.bottomlessarchive.loa.stage.view.document.controller;
 
 import com.github.bottomlessarchive.loa.stage.configuration.StagingConfigurationProperties;
-import com.github.bottomlessarchive.loa.stage.util.AutoDeleteFileSystemResource;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
-import org.springframework.core.io.Resource;
+import org.springframework.http.ZeroCopyHttpOutputMessage;
+import org.springframework.http.codec.multipart.FilePart;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
+import reactor.core.publisher.Mono;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 @RestController
@@ -22,12 +25,27 @@ public class StageDocumentController {
 
     @SneakyThrows
     @PostMapping("/document/{documentId}")
-    public void persistDocument(@PathVariable final String documentId, @RequestParam("file") final MultipartFile file) {
-        file.transferTo(Path.of(stagingConfigurationProperties.path()).resolve(documentId));
+    public Mono<Void> persistDocument(@PathVariable final String documentId,
+            @RequestPart("file") final Mono<FilePart> file) {
+        return file.flatMap(p -> p.transferTo(Path.of(stagingConfigurationProperties.path()).resolve(documentId)))
+                .then();
     }
 
     @GetMapping("/document/{documentId}")
-    public Resource serveDocument(@PathVariable final String documentId) {
-        return new AutoDeleteFileSystemResource(Path.of(stagingConfigurationProperties.path()).resolve(documentId));
+    public Mono<Void> serveDocument(@PathVariable final String documentId, ServerHttpResponse response) throws IOException {
+        ZeroCopyHttpOutputMessage zeroCopyResponse = (ZeroCopyHttpOutputMessage) response;
+
+        Path file = Path.of(stagingConfigurationProperties.path()).resolve(documentId);
+
+        return zeroCopyResponse.writeWith(file, 0, Files.size(file))
+                .then(
+                        Mono.fromRunnable(() -> {
+                            try {
+                                Files.deleteIfExists(file);
+                            } catch (IOException e) {
+                                // Do nothing
+                            }
+                        })
+                );
     }
 }
