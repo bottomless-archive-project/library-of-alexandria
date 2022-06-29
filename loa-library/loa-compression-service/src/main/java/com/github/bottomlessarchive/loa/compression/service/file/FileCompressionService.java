7f0c9b39bfee56a1aacbd3a65e44a42d852477f3
laxika <laxika91@gmail.com>
2022-06-29 23:48:01 +0200

    #369 - Fixed an error.

--- loa-library/loa-compression-service/src/main/java/com/github/bottomlessarchive/loa/compression/service/file/FileCompressionService.java
+++ loa-library/loa-compression-service/src/main/java/com/github/bottomlessarchive/loa/compression/service/file/FileCompressionService.java
@@ -1,14 +1,11 @@
 package com.github.bottomlessarchive.loa.compression.service.file;
 
 import com.github.bottomlessarchive.loa.compression.configuration.CompressionConfigurationProperties;
-import com.github.bottomlessarchive.loa.compression.domain.CompressionException;
 import com.github.bottomlessarchive.loa.compression.domain.DocumentCompression;
 import com.github.bottomlessarchive.loa.compression.service.compressor.provider.CompressorServiceProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 
 @Service
@@ -23,15 +20,7 @@ public class FileCompressionService {
             return fileLocation;
         }
 
-        try {
-            final Path compressedDocumentLocation = compressorServiceProvider.getCompressionService(
-                    compressionConfigurationProperties.algorithm()).compress(fileLocation);
-
-            Files.delete(fileLocation);
-
-            return compressedDocumentLocation;
-        } catch (IOException e) {
-            throw new CompressionException("Failed to compress file!", e);
-        }
+        return compressorServiceProvider.getCompressionService(
+                compressionConfigurationProperties.algorithm()).compress(fileLocation);
     }
 }
