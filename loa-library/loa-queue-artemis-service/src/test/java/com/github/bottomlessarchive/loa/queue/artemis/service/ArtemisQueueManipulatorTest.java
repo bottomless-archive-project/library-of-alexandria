6599beb0c9c9b02867319027d245b26b101e36ec
laxika <laxika91@gmail.com>
2022-07-19 20:47:16 +0200

    #430 - Fixed the existing tests.

--- loa-library/loa-queue-artemis-service/src/test/java/com/github/bottomlessarchive/loa/queue/artemis/service/ArtemisQueueManipulatorTest.java
+++ loa-library/loa-queue-artemis-service/src/test/java/com/github/bottomlessarchive/loa/queue/artemis/service/ArtemisQueueManipulatorTest.java
@@ -2,38 +2,34 @@ package com.github.bottomlessarchive.loa.queue.artemis.service;
 
 import com.github.bottomlessarchive.loa.queue.artemis.service.consumer.deserializer.MessageDeserializer;
 import com.github.bottomlessarchive.loa.queue.artemis.service.consumer.deserializer.MessageDeserializerProvider;
+import com.github.bottomlessarchive.loa.queue.artemis.service.consumer.pool.QueueConsumerProvider;
+import com.github.bottomlessarchive.loa.queue.artemis.service.consumer.pool.domain.QueueConsumer;
 import com.github.bottomlessarchive.loa.queue.service.domain.Queue;
 import com.github.bottomlessarchive.loa.queue.service.domain.QueueException;
 import org.apache.activemq.artemis.api.core.ActiveMQException;
-import org.apache.activemq.artemis.api.core.ActiveMQLargeMessageInterruptedException;
 import org.apache.activemq.artemis.api.core.client.ClientConsumer;
 import org.apache.activemq.artemis.api.core.client.ClientMessage;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
-import java.util.function.Function;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-@Disabled
 @ExtendWith(MockitoExtension.class)
 class ArtemisQueueManipulatorTest {
 
     @Mock
     private MessageDeserializerProvider messageDeserializerProvider;
 
-    @Captor
-    private ArgumentCaptor<Function<ClientConsumer, String>> clientConsumerArgumentCaptor;
+    @Mock
+    private QueueConsumerProvider queueConsumerProvider;
 
     @InjectMocks
     private ArtemisQueueManipulator artemisQueueManipulator;
@@ -41,17 +37,12 @@ class ArtemisQueueManipulatorTest {
     @Test
     @SuppressWarnings("unchecked")
     void testReadMessageWhenReadWasSuccessful() throws ActiveMQException {
-        /*when(clientConsumerExecutor.invokeConsumer(eq(Queue.DOCUMENT_ARCHIVING_QUEUE), clientConsumerArgumentCaptor.capture()))
-                .thenReturn("test-result-message");*/
-
-        final String result = artemisQueueManipulator.readMessage(Queue.DOCUMENT_ARCHIVING_QUEUE, String.class);
-
-        assertThat(result)
-                .isEqualTo("test-result-message");
-
-        final Function<ClientConsumer, String> resultFunction = clientConsumerArgumentCaptor.getValue();
-
         final ClientConsumer clientConsumer = mock(ClientConsumer.class);
+        final QueueConsumer queueConsumer = QueueConsumer.builder()
+                .clientConsumer(clientConsumer)
+                .build();
+        when(queueConsumerProvider.getConsumer(Queue.DOCUMENT_ARCHIVING_QUEUE))
+                .thenReturn(queueConsumer);
         final ClientMessage clientMessage = mock(ClientMessage.class);
         when(clientConsumer.receive())
                 .thenReturn(clientMessage);
@@ -62,55 +53,23 @@ class ArtemisQueueManipulatorTest {
         when(messageDeserializer.deserialize(clientMessage))
                 .thenReturn("function-result");
 
-        final String functionResult = resultFunction.apply(clientConsumer);
+        final String result = artemisQueueManipulator.readMessage(Queue.DOCUMENT_ARCHIVING_QUEUE, String.class);
 
-        assertThat(functionResult)
+        assertThat(result)
                 .isEqualTo("function-result");
     }
 
     @Test
-    @SuppressWarnings("unchecked")
-    void testReadMessageWhenLargeMessageExceptionHappen() throws ActiveMQException {
-        /*when(clientConsumerExecutor.invokeConsumer(eq(Queue.DOCUMENT_ARCHIVING_QUEUE), clientConsumerArgumentCaptor.capture()))
-                .thenReturn("test-result-message");*/
-
-        final String result = artemisQueueManipulator.readMessage(Queue.DOCUMENT_ARCHIVING_QUEUE, String.class);
-
-        assertThat(result)
-                .isEqualTo("test-result-message");
-
-        final Function<ClientConsumer, String> resultFunction = clientConsumerArgumentCaptor.getValue();
-
-        final ClientConsumer clientConsumer = mock(ClientConsumer.class);
-        final ClientMessage clientMessage = mock(ClientMessage.class);
-        when(clientConsumer.receive())
-                .thenReturn(clientMessage);
-
-        final MessageDeserializer<String> messageDeserializer = mock(MessageDeserializer.class);
-        when(messageDeserializerProvider.getDeserializer(Queue.DOCUMENT_ARCHIVING_QUEUE))
-                .thenReturn(Optional.of(messageDeserializer));
-        when(messageDeserializer.deserialize(clientMessage))
-                .thenThrow(new RuntimeException("runtime-error", new ActiveMQLargeMessageInterruptedException("large-message-error")));
-
-        assertThrows(QueueException.class, () -> resultFunction.apply(clientConsumer));
-    }
-
-    @Test
     void testReadMessageWhenActiveMQExceptionHappen() throws ActiveMQException {
-        /*when(clientConsumerExecutor.invokeConsumer(eq(Queue.DOCUMENT_ARCHIVING_QUEUE), clientConsumerArgumentCaptor.capture()))
-                .thenReturn("test-result-message");*/
-
-        final String result = artemisQueueManipulator.readMessage(Queue.DOCUMENT_ARCHIVING_QUEUE, String.class);
-
-        assertThat(result)
-                .isEqualTo("test-result-message");
-
-        final Function<ClientConsumer, String> resultFunction = clientConsumerArgumentCaptor.getValue();
-
         final ClientConsumer clientConsumer = mock(ClientConsumer.class);
+        final QueueConsumer queueConsumer = QueueConsumer.builder()
+                .clientConsumer(clientConsumer)
+                .build();
+        when(queueConsumerProvider.getConsumer(Queue.DOCUMENT_ARCHIVING_QUEUE))
+                .thenReturn(queueConsumer);
         when(clientConsumer.receive())
                 .thenThrow(new ActiveMQException("large-message-error"));
 
-        assertThrows(QueueException.class, () -> resultFunction.apply(clientConsumer));
+        assertThrows(QueueException.class, () -> artemisQueueManipulator.readMessage(Queue.DOCUMENT_ARCHIVING_QUEUE, String.class));
     }
 }
