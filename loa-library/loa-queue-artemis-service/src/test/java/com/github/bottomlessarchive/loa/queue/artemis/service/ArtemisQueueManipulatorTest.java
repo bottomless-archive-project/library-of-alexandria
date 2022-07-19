7787cc8fa264b55c0dc0833f3ea290d8281e6fb0
laxika <laxika91@gmail.com>
2022-07-19 21:09:31 +0200

    #430 - Added more test for ArtemisQueueManipulator.

--- loa-library/loa-queue-artemis-service/src/test/java/com/github/bottomlessarchive/loa/queue/artemis/service/ArtemisQueueManipulatorTest.java
+++ loa-library/loa-queue-artemis-service/src/test/java/com/github/bottomlessarchive/loa/queue/artemis/service/ArtemisQueueManipulatorTest.java
@@ -4,11 +4,16 @@ import com.github.bottomlessarchive.loa.queue.artemis.service.consumer.deseriali
 import com.github.bottomlessarchive.loa.queue.artemis.service.consumer.deserializer.MessageDeserializerProvider;
 import com.github.bottomlessarchive.loa.queue.artemis.service.consumer.pool.QueueConsumerProvider;
 import com.github.bottomlessarchive.loa.queue.artemis.service.consumer.pool.domain.QueueConsumer;
+import com.github.bottomlessarchive.loa.queue.artemis.service.producer.pool.QueueProducerProvider;
+import com.github.bottomlessarchive.loa.queue.artemis.service.producer.pool.domain.QueueProducer;
+import com.github.bottomlessarchive.loa.queue.artemis.service.producer.serializer.MessageSerializer;
+import com.github.bottomlessarchive.loa.queue.artemis.service.producer.serializer.MessageSerializerProvider;
 import com.github.bottomlessarchive.loa.queue.service.domain.Queue;
 import com.github.bottomlessarchive.loa.queue.service.domain.QueueException;
 import org.apache.activemq.artemis.api.core.ActiveMQException;
 import org.apache.activemq.artemis.api.core.client.ClientConsumer;
 import org.apache.activemq.artemis.api.core.client.ClientMessage;
+import org.apache.activemq.artemis.api.core.client.ClientProducer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -19,11 +24,16 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class ArtemisQueueManipulatorTest {
+
+    @Mock
+    private MessageSerializerProvider messageSerializerProvider;
 
     @Mock
     private MessageDeserializerProvider messageDeserializerProvider;
@@ -31,8 +41,11 @@ class ArtemisQueueManipulatorTest {
     @Mock
     private QueueConsumerProvider queueConsumerProvider;
 
+    @Mock
+    private QueueProducerProvider queueProducerProvider;
+
     @InjectMocks
-    private ArtemisQueueManipulator artemisQueueManipulator;
+    private ArtemisQueueManipulator underTest;
 
     @Test
     @SuppressWarnings("unchecked")
@@ -53,7 +66,7 @@ class ArtemisQueueManipulatorTest {
         when(messageDeserializer.deserialize(clientMessage))
                 .thenReturn("function-result");
 
-        final String result = artemisQueueManipulator.readMessage(Queue.DOCUMENT_ARCHIVING_QUEUE, String.class);
+        final String result = underTest.readMessage(Queue.DOCUMENT_ARCHIVING_QUEUE, String.class);
 
         assertThat(result)
                 .isEqualTo("function-result");
@@ -70,6 +83,59 @@ class ArtemisQueueManipulatorTest {
         when(clientConsumer.receive())
                 .thenThrow(new ActiveMQException("large-message-error"));
 
-        assertThrows(QueueException.class, () -> artemisQueueManipulator.readMessage(Queue.DOCUMENT_ARCHIVING_QUEUE, String.class));
+        assertThrows(QueueException.class, () -> underTest.readMessage(Queue.DOCUMENT_ARCHIVING_QUEUE, String.class));
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    void testSendMessageWhenSendWasSuccessful() throws ActiveMQException {
+        final MessageSerializer<Object> messageSerializer = mock(MessageSerializer.class);
+        when(messageSerializerProvider.getSerializer(Queue.DOCUMENT_ARCHIVING_QUEUE))
+                .thenReturn(Optional.of(messageSerializer));
+        final Object message = new Object();
+        final ClientMessage clientMessage = mock(ClientMessage.class);
+        when(messageSerializer.serialize(message))
+                .thenReturn(clientMessage);
+        final ClientProducer clientProducer = mock(ClientProducer.class);
+        final QueueProducer queueProducer = QueueProducer.builder()
+                .clientProducer(clientProducer)
+                .build();
+        when(queueProducerProvider.getProducer(Queue.DOCUMENT_ARCHIVING_QUEUE))
+                .thenReturn(queueProducer);
+
+        underTest.sendMessage(Queue.DOCUMENT_ARCHIVING_QUEUE, message);
+
+        verify(clientProducer).send(clientMessage);
+    }
+
+    @Test
+    void testSendMessageWhenMessageSerializerWasNotFound() {
+        when(messageSerializerProvider.getSerializer(Queue.DOCUMENT_ARCHIVING_QUEUE))
+                .thenReturn(Optional.empty());
+
+        assertThrows(QueueException.class, () -> underTest.sendMessage(Queue.DOCUMENT_ARCHIVING_QUEUE, new Object()));
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    void testSendMessageWhenSendWasUnsuccessful() throws ActiveMQException {
+        final MessageSerializer<Object> messageSerializer = mock(MessageSerializer.class);
+        when(messageSerializerProvider.getSerializer(Queue.DOCUMENT_ARCHIVING_QUEUE))
+                .thenReturn(Optional.of(messageSerializer));
+        final Object message = new Object();
+        final ClientMessage clientMessage = mock(ClientMessage.class);
+        when(messageSerializer.serialize(message))
+                .thenReturn(clientMessage);
+        final ClientProducer clientProducer = mock(ClientProducer.class);
+        final QueueProducer queueProducer = QueueProducer.builder()
+                .clientProducer(clientProducer)
+                .build();
+        when(queueProducerProvider.getProducer(Queue.DOCUMENT_ARCHIVING_QUEUE))
+                .thenReturn(queueProducer);
+        doThrow(new QueueException("Test exception."))
+                .when(clientProducer)
+                .send(clientMessage);
+
+        assertThrows(QueueException.class, () -> underTest.sendMessage(Queue.DOCUMENT_ARCHIVING_QUEUE, message));
     }
 }
