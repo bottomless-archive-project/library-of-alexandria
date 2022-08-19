6aa6bd73279b79c9e0fec078928b422c956d7645
laxika <laxika91@gmail.com>
2022-08-19 19:27:20 +0200

    #455 - Fixed that the parallelism in the Downloader Application didn't work correctly after 64 threads.

--- loa-application/loa-downloader-application/src/main/java/com/github/bottomlessarchive/loa/downloader/configuration/DownloaderClientConfiguration.java
+++ loa-application/loa-downloader-application/src/main/java/com/github/bottomlessarchive/loa/downloader/configuration/DownloaderClientConfiguration.java
@@ -1,15 +1,38 @@
 package com.github.bottomlessarchive.loa.downloader.configuration;
 
+import lombok.RequiredArgsConstructor;
+import okhttp3.ConnectionPool;
+import okhttp3.Dispatcher;
 import okhttp3.OkHttpClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.concurrent.TimeUnit;
+
 @Configuration
+@RequiredArgsConstructor
 public class DownloaderClientConfiguration {
+
+    private final DownloaderConfigurationProperties downloaderConfigurationProperties;
 
     @Bean
     public OkHttpClient downloaderClient() {
         return new OkHttpClient.Builder()
+                .connectionPool(connectionPool())
+                .dispatcher(dispatcher())
                 .build();
+    }
+
+    private Dispatcher dispatcher() {
+        final Dispatcher dispatcher = new Dispatcher();
+
+        dispatcher.setMaxRequests(downloaderConfigurationProperties.parallelism());
+        dispatcher.setMaxRequestsPerHost(10);
+
+        return dispatcher;
+    }
+
+    private ConnectionPool connectionPool() {
+        return new ConnectionPool(downloaderConfigurationProperties.parallelism(), 5, TimeUnit.MINUTES);
     }
 }
