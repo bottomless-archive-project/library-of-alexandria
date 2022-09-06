f91355f97ed07c8bae628c9532c79f36ab124711
laxika <laxika91@gmail.com>
2022-09-06 23:50:29 +0200

    #465 - Added duplicate detection to the Downloader Application.

--- loa-application/loa-downloader-application/src/main/java/com/github/bottomlessarchive/loa/downloader/service/document/DocumentLocationProcessor.java
+++ loa-application/loa-downloader-application/src/main/java/com/github/bottomlessarchive/loa/downloader/service/document/DocumentLocationProcessor.java
@@ -1,10 +1,14 @@
 package com.github.bottomlessarchive.loa.downloader.service.document;
 
+import com.github.bottomlessarchive.loa.checksum.service.ChecksumProvider;
 import com.github.bottomlessarchive.loa.document.service.DocumentTypeCalculator;
+import com.github.bottomlessarchive.loa.document.service.domain.DocumentEntity;
 import com.github.bottomlessarchive.loa.document.service.domain.DocumentType;
+import com.github.bottomlessarchive.loa.document.service.entity.factory.DocumentEntityFactory;
 import com.github.bottomlessarchive.loa.downloader.service.document.domain.DocumentArchivingContext;
 import com.github.bottomlessarchive.loa.downloader.service.document.domain.exception.NotEnoughSpaceException;
 import com.github.bottomlessarchive.loa.downloader.service.file.FileCollector;
+import com.github.bottomlessarchive.loa.file.FileManipulatorService;
 import com.github.bottomlessarchive.loa.location.domain.DocumentLocation;
 import com.github.bottomlessarchive.loa.stage.service.StageLocationFactory;
 import com.github.bottomlessarchive.loa.stage.service.domain.StageLocation;
@@ -36,7 +40,11 @@ public class DocumentLocationProcessor {
     private final DocumentFileValidator documentFileValidator;
     private final FileCollector fileCollector;
     private final DocumentArchiver documentArchiver;
+
+    private final ChecksumProvider checksumProvider;
     private final DocumentTypeCalculator documentTypeCalculator;
+    private final DocumentEntityFactory documentEntityFactory;
+    private final FileManipulatorService fileManipulatorService;
     private final FileValidationConfigurationProperties fileValidationConfigurationProperties;
 
     @Qualifier("downloaderSemaphore")
@@ -98,6 +106,19 @@ public class DocumentLocationProcessor {
         try {
             fileCollector.acquireFile(documentLocation.getId(), documentLocationURL, stageLocation.getPath(), documentType);
 
+            final String checksum = checksumProvider.checksum(fileManipulatorService.getInputStream(stageLocation.getPath()));
+            final long contentLength = fileManipulatorService.size(stageLocation.getPath());
+
+            final Optional<DocumentEntity> documentEntityOptional = documentEntityFactory.getDocumentEntity(
+                    checksum, contentLength, documentType.toString());
+            if (documentEntityOptional.isPresent()) {
+                log.info("Document with id: {} is a duplicate.", documentId);
+
+                documentEntityFactory.addSourceLocation(documentEntityOptional.get().getId(), documentLocation.getId());
+
+                return;
+            }
+
             if (documentFileValidator.isValidDocument(documentId.toString(), documentType)) {
                 final DocumentArchivingContext documentArchivingContext = DocumentArchivingContext.builder()
                         .id(documentId)
@@ -113,10 +134,10 @@ public class DocumentLocationProcessor {
             }
         } catch (final Exception e) {
             log.info("Error downloading a document: {}!", e.getMessage());
-        }
-
-        if (stageLocation.exists()) {
-            stageLocation.cleanup();
+        } finally {
+            if (stageLocation.exists()) {
+                stageLocation.cleanup();
+            }
         }
     }
 }
