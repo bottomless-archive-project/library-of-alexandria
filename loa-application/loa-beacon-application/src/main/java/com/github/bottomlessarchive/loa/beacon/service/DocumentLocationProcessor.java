2eb718f51bdd45feae5b54af669408b5d16e951e
laxika <laxika91@gmail.com>
2022-09-23 17:35:10 +0200

    #463 - Added parallelism to the beacon and updated to JDK 19.

--- loa-application/loa-beacon-application/src/main/java/com/github/bottomlessarchive/loa/beacon/service/DocumentLocationProcessor.java
+++ loa-application/loa-beacon-application/src/main/java/com/github/bottomlessarchive/loa/beacon/service/DocumentLocationProcessor.java
@@ -2,11 +2,16 @@ package com.github.bottomlessarchive.loa.beacon.service;
 
 import com.github.bottomlessarchive.loa.beacon.service.domain.DocumentLocation;
 import com.github.bottomlessarchive.loa.beacon.service.domain.DocumentLocationResult;
+import jdk.incubator.concurrent.StructuredTaskScope;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class DocumentLocationProcessor {
@@ -14,8 +19,27 @@ public class DocumentLocationProcessor {
     private final DocumentLocationVisitor documentLocationVisitor;
 
     public List<DocumentLocationResult> processLocations(final List<DocumentLocation> documentLocations) {
-        return documentLocations.stream()
-                .map(documentLocationVisitor::visitDocumentLocation)
-                .toList();
+        try (var scope = new StructuredTaskScope.ShutdownOnFailure()) {
+            List<Future<DocumentLocationResult>> results = documentLocations.stream()
+                    .map(documentLocation -> scope.fork(() -> visitDocumentLocation(documentLocation)))
+                    .toList();
+
+            scope.join();
+            scope.throwIfFailed();
+
+            return results.stream()
+                    .map(Future::resultNow)
+                    .toList();
+        } catch (ExecutionException | InterruptedException e) {
+            throw new RuntimeException(e); //TODO
+        }
+    }
+
+    private DocumentLocationResult visitDocumentLocation(final DocumentLocation documentLocation) {
+        final DocumentLocationResult result = documentLocationVisitor.visitDocumentLocation(documentLocation);
+
+        log.info("Finished visiting url: {}.", documentLocation.getLocation());
+
+        return result;
     }
 }
