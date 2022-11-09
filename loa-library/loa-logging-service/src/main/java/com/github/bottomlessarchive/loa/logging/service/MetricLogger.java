eba37f03ba4ba12200d3271bb222d484a574d058
laxika <laxika91@gmail.com>
2022-11-09 23:22:50 +0100

    #475 - The Downloader Application shuts down after it processed the whole folder when loa.downloader.source is FOLDER.

--- loa-library/loa-logging-service/src/main/java/com/github/bottomlessarchive/loa/logging/service/MetricLogger.java
+++ loa-library/loa-logging-service/src/main/java/com/github/bottomlessarchive/loa/logging/service/MetricLogger.java
@@ -4,9 +4,16 @@ import io.micrometer.core.instrument.Counter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+
 @Slf4j
 @Service
 public class MetricLogger {
+
+    public void logCounters(final Counter... counter) {
+        Arrays.stream(counter)
+                .forEach(this::logCounter);
+    }
 
     public void logCounter(final Counter counter) {
         if (log.isInfoEnabled()) {
