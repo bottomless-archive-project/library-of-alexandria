733295058087f131ba8b5c6091b42ff8ddd79d95
laxika <laxika91@gmail.com>
2023-01-23 15:55:41 +0100

    #474 - Fixed the existing tests in StageLocationFactoryTest and added new ones to cover the folder auto-creating logic.

--- loa-library/loa-stage-service/src/test/java/com/github/bottomlessarchive/loa/stage/service/StageLocationFactoryTest.java
+++ loa-library/loa-stage-service/src/test/java/com/github/bottomlessarchive/loa/stage/service/StageLocationFactoryTest.java
@@ -2,45 +2,64 @@ package com.github.bottomlessarchive.loa.stage.service;
 
 import com.github.bottomlessarchive.loa.stage.configuration.StageConfigurationProperties;
 import com.github.bottomlessarchive.loa.stage.service.domain.StageLocation;
+import com.google.common.jimfs.Configuration;
+import com.google.common.jimfs.Jimfs;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.io.IOException;
+import java.nio.file.FileSystem;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class StageLocationFactoryTest {
 
     private static final UUID TEST_DOCUMENT_ID = UUID.fromString("123e4567-e89b-42d3-a456-556642440000");
 
-    private StageLocationFactory underTest;
-
-    @Mock
     private Path stageLocation;
+    private StageLocationFactory underTest;
+    private FileSystem fileSystem;
 
     @BeforeEach
     void setup() {
+        fileSystem = Jimfs.newFileSystem(Configuration.unix());
+
+        stageLocation = fileSystem.getPath("/stage/location");
+
         underTest = new StageLocationFactory(
                 new StageConfigurationProperties(stageLocation)
         );
     }
 
+    @AfterEach
+    void teardown() throws IOException {
+        fileSystem.close();
+    }
+
     @Test
-    void testGetLocation() {
-        final Path mockPath = mock(Path.class);
-        when(stageLocation.resolve("123e4567-e89b-42d3-a456-556642440000"))
-                .thenReturn(mockPath);
+    void testGetLocationWhenLocationExists() throws IOException {
+        Files.createDirectories(stageLocation);
 
         final StageLocation result = underTest.getLocation(TEST_DOCUMENT_ID);
 
         assertThat(result.getPath())
-                .isSameAs(mockPath);
+                .isEqualTo(fileSystem.getPath("/stage/location/123e4567-e89b-42d3-a456-556642440000"));
+    }
+
+    @Test
+    void testGetLocationWhenLocationDoesNotExists() {
+        final StageLocation result = underTest.getLocation(TEST_DOCUMENT_ID);
+
+        assertThat(result.getPath())
+                .isEqualTo(fileSystem.getPath("/stage/location/123e4567-e89b-42d3-a456-556642440000"));
+        assertThat(fileSystem.getPath("/stage/location/"))
+                .exists();
     }
 }
