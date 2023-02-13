a4bfc41171a31c4cfb9f39ed77fb85306ef1c3c1
laxika <laxika91@gmail.com>
2023-02-13 17:45:52 +0100

    #500 - Trying to fix the build on  GitHub.

--- loa-library/loa-conductor-client-service/src/main/java/com/github/bottomlessarchive/loa/conductor/service/InstanceRefreshSchedulerService.java
+++ loa-library/loa-conductor-client-service/src/main/java/com/github/bottomlessarchive/loa/conductor/service/InstanceRefreshSchedulerService.java
@@ -3,7 +3,6 @@ package com.github.bottomlessarchive.loa.conductor.service;
 import com.github.bottomlessarchive.loa.conductor.service.client.ConductorClient;
 import com.github.bottomlessarchive.loa.conductor.service.client.configuration.ConductorClientConfigurationProperties;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
