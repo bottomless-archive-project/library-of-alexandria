9b2bb4ae00af1172b015c26a28f5e10d2efed227
laxika <laxika91@gmail.com>
2023-04-10 00:38:43 +0200

    #512 - Replaced the deprecated Angular resolvers.

--- loa-frontend/src/app/app-routing.module.ts
+++ loa-frontend/src/app/app-routing.module.ts
@@ -1,23 +1,35 @@
-import {NgModule} from '@angular/core';
+import {inject, NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './view/home/home.component';
 import {SearchComponent} from './view/search/search.component';
 import {DashboardStatisticsComponent} from './view/dashboard-statistics/dashboard-statistics.component';
-import {SearchResolver} from './shared/search/resolver/search.resolver';
-import {StatisticsResolver} from './shared/statistics/resolver/statistics-resolver';
 import {DebugDocumentComponent} from './view/debug-document/debug-document.component';
 import {DebugLocationComponent} from './view/debug-location/debug-location.component';
-import {ApplicationResolver} from './shared/application/resolver/application-resolver';
 import {DashboardApplicationsComponent} from './view/dashboard-applications/dashboard-applications.component';
 import {LoginComponent} from './view/login/login.component';
+import {StatisticsService} from './shared/statistics/service/statistics-service';
+import {SearchService} from './shared/search/service/search.service';
+import {ApplicationService} from './shared/application/service/application.service';
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'search', component: SearchComponent, resolve: {statistics: SearchResolver}},
-  {path: 'dashboard/statistics', component: DashboardStatisticsComponent, resolve: {statistics: StatisticsResolver}},
-  {path: 'dashboard/applications', component: DashboardApplicationsComponent, resolve: {applications: ApplicationResolver}},
+  {
+    path: 'search',
+    component: SearchComponent,
+    resolve: {statistics: () => inject(SearchService).getSearchStatistics()}
+  },
+  {
+    path: 'dashboard/statistics',
+    component: DashboardStatisticsComponent,
+    resolve: {statistics: () => inject(StatisticsService).getStatistics()}
+  },
+  {
+    path: 'dashboard/applications',
+    component: DashboardApplicationsComponent,
+    resolve: {applications: () => inject(ApplicationService).getApplications()}
+  },
   {path: 'debug-document', component: DebugDocumentComponent},
   {path: 'debug-location', component: DebugLocationComponent}
 ];
